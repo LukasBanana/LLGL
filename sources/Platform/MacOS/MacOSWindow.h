8fefddc34b2ceb1b2ecf89b74bb6687b84f93456
Lukas Hermanns <lhlukash23@googlemail.com>
2016-07-09 21:08:48 +0200

    Added (provisional) MacOSWindow class.

---
+++ sources/Platform/MacOS/MacOSWindow.h
@@ -0,0 +1,65 @@
+/*
+ * Win32Window.h
+ * 
+ * This file is part of the "LLGL" project (Copyright (c) 2015 by Lukas Hermanns)
+ * See "LICENSE.txt" for license information.
+ */
+
+#ifndef __LLGL_WIN32_WINDOW_H__
+#define __LLGL_WIN32_WINDOW_H__
+
+
+#include <LLGL/Window.h>
+#include <Windows.h>
+
+
+namespace LLGL
+{
+
+
+class Win32Window : public Window
+{
+
+    public:
+
+        Win32Window(const WindowDesc& desc);
+        ~Win32Window();
+
+        void SetPosition(int x, int y) override;
+        void GetPosition(int& x, int& y) const override;
+
+        void SetSize(int width, int height, bool useClientArea = true) override;
+        void GetSize(int& width, int& height, bool useClientArea = true) const override;
+
+        void SetTitle(const std::wstring& title) override;
+        std::wstring GetTitle() const override;
+
+        void Show(bool show = true) override;
+        bool IsShown() const override;
+
+        const void* GetNativeHandle() const override;
+
+    private:
+        
+        void ProcessSystemEvents() override;
+
+        HWND CreateWindowHandle(const WindowDesc& desc);
+
+        void SetUserData(void* userData);
+
+        WindowDesc  desc_;
+
+        HWND        wnd_;
+        HDC         dc_;
+
+};
+
+
+} // /namespace LLGL
+
+
+#endif
+
+
+
+// ================================================================================
