8815955a9a3748c89efe77a9d0c3c8f0ac4122b8
Lukas Hermanns <lukas.hermanns90@gmail.com>
2016-08-20 12:50:28 +0200

    Fixed assertion for g++.

--- sources/Renderer/Assertion.h
+++ sources/Renderer/Assertion.h
@@ -10,18 +10,27 @@
 
 
 #include <stdexcept>
+#include <string>
 
 
 namespace LLGL
 {
 
 
-#define LLGL_ASSERT_PTR(PTR)                                                    \
-    if (!PTR)                                                                   \
-    {                                                                           \
-        throw std::invalid_argument(                                            \
-            __FUNCTION__ ": null pointer exception of parameter \"" #PTR "\""   \
-        );                                                                      \
+#ifdef _MSC_VER
+#   define LLGL_ASSERT_INFO(INFO) (__FUNCTION__ ": " INFO)
+#else
+#   define LLGL_ASSERT_INFO(INFO) (std::string(__FUNCTION__) + std::string(": ") + std::string(INFO))
+#endif
+
+#define LLGL_ASSERT_PTR(PTR)                                                \
+    if (!PTR)                                                               \
+    {                                                                       \
+        throw std::invalid_argument(                                        \
+            std::string(__FUNCTION__) +                                     \
+                std::string(": null pointer exception of parameter \"") +   \
+                std::string(#PTR) + std::string("\"")                       \
+        );                                                                  \
     }
 
 
