0c87d06969a881a8da1c11e7940d7667d36bfef4
Lukas Hermanns <lukas.hermanns90@gmail.com>
2016-10-03 11:35:28 +0200

    Added constructors to VertexAttribute structure.

--- include/LLGL/VertexAttribute.h
+++ include/LLGL/VertexAttribute.h
@@ -9,6 +9,7 @@
 #define __LLGL_VERTEX_ATTRIBUTE_H__
 
 
+#include "Export.h"
 #include "Image.h"
 #include <string>
 
@@ -18,15 +19,61 @@ namespace LLGL
 
 
 //! Vertex attribute class.
-struct VertexAttribute
+struct LLGL_EXPORT VertexAttribute
 {
+    VertexAttribute() = default;
+    VertexAttribute(const VertexAttribute&) = default;
+    VertexAttribute& operator = (const VertexAttribute&) = default;
+
+    /**
+    \brief Constructs a vertex attribute with a specified name (used for GLSL).
+    \param[in] name Specifies the attribute name (for GLSL).
+    \param[in] dataType Specifies the data type of the attribute components.
+    \param[in] components Specifies the number of attribute components. This must be 1, 2, 3, or 4.
+    \param[in] instanceDivisor Specifies the divisor (or step rate) for instance data.
+    If this is 0, this vertex attribute is considered to be per-vertex. By default 0.
+    \remarks This is equivalent to:
+    \code
+    VertexAttribute(name, 0, dataType, components, instanceDivisor);
+    \endcode
+    */
+    VertexAttribute(
+        const std::string& name,
+        const DataType dataType,
+        unsigned int components,
+        unsigned int instanceDivisor = 0
+    );
+
+    /**
+    \brief Constructs a vertex attribute with a specified semantic (used for HLSL).
+    \param[in] semanticName Specifies the semantic name (for HLSL).
+    \param[in] semanticIndex Specifies the semantic index (for HLSL).
+    \param[in] dataType Specifies the data type of the attribute components.
+    \param[in] components Specifies the number of attribute components. This must be 1, 2, 3, or 4.
+    \param[in] instanceDivisor Specifies the divisor (or step rate) for instance data.
+    If this is 0, this vertex attribute is considered to be per-vertex. By default 0.
+    \remarks This is equivalent to:
+    \code
+    VertexAttribute(name, 0, dataType, components, instanceDivisor);
+    \endcode
+    */
+    VertexAttribute(
+        const std::string& semanticName,
+        unsigned int semanticIndex,
+        const DataType dataType,
+        unsigned int components,
+        unsigned int instanceDivisor = 0
+    );
+
+    //! Vertex attribute name (for GLSL) or semantic name (for HLSL).
+    std::string     name;
+
     //! Data type of the vertex attribute components. By default DataType::Float.
     DataType        dataType        = DataType::Float;
 
-    //! Specifies whether non-floating-point data types are to be converted to floating-points. By default false.
-    bool            conversion      = false;
+    //! Number of components: 1, 2, 3, or 4. By default 4.
+    unsigned int    components      = 4;
 
-    //! Specifies whether this is a per-instance data. If false, this is a per-vertex data.
     /**
     \brief Specifies the instance data divosor (or instance data step rate).
     \remarks If this is 0, this attribute is considered to be stored per vertex.
@@ -34,14 +81,11 @@ struct VertexAttribute
     */
     unsigned int    instanceDivisor = 0;
 
-    //! Number of components: 1, 2, 3, or 4. By default 4.
-    unsigned int    components      = 4;
+    //! Specifies whether non-floating-point data types are to be converted to floating-points. By default false.
+    bool            conversion      = false;
 
     //! Byte offset within each vertex. By default 0.
     unsigned int    offset          = 0;
-
-    //! Vertex attribute name (for GLSL) or semantic name (for HLSL).
-    std::string     name;
 
     //! Semantic index (only relevant for HLSL).
     unsigned int    semanticIndex   = 0;
--- sources/Core/VertexAttribute.cpp
+++ sources/Core/VertexAttribute.cpp
@@ -13,16 +13,33 @@ namespace LLGL
 {
 
 
+VertexAttribute::VertexAttribute(
+    const std::string& name, const DataType dataType, unsigned int components, unsigned int instanceDivisor) :
+        VertexAttribute( name, 0, dataType, components, instanceDivisor )
+{
+}
+
+VertexAttribute::VertexAttribute(
+    const std::string& semanticName, unsigned int semanticIndex, const DataType dataType, unsigned int components, unsigned int instanceDivisor) :
+        name            ( semanticName    ),
+        dataType        ( dataType        ),
+        components      ( components      ),
+        instanceDivisor ( instanceDivisor ),
+        semanticIndex   ( semanticIndex   )
+{
+}
+
+
 LLGL_EXPORT bool operator == (const VertexAttribute& lhs, const VertexAttribute& rhs)
 {
     return
     (
+        LLGL_COMPARE_MEMBER_EQ( name            ) &&
         LLGL_COMPARE_MEMBER_EQ( dataType        ) &&
+        LLGL_COMPARE_MEMBER_EQ( components      ) &&
         LLGL_COMPARE_MEMBER_EQ( conversion      ) &&
         LLGL_COMPARE_MEMBER_EQ( instanceDivisor ) &&
-        LLGL_COMPARE_MEMBER_EQ( components      ) &&
         LLGL_COMPARE_MEMBER_EQ( offset          ) &&
-        LLGL_COMPARE_MEMBER_EQ( name            ) &&
         LLGL_COMPARE_MEMBER_EQ( semanticIndex   )
     );
 }
