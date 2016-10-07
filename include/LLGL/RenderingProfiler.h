73741c69f6537189ccc3c1f67f9419dd86dc26a4
Lukas Hermanns <lukas.hermanns90@gmail.com>
2016-10-07 18:01:45 +0200

    Continued with docu.

--- include/LLGL/RenderingProfiler.h
+++ include/LLGL/RenderingProfiler.h
@@ -27,6 +27,7 @@ class LLGL_EXPORT RenderingProfiler
 
     public:
 
+        //! Profiling counter class.
         class LLGL_EXPORT Counter
         {
 
@@ -34,26 +35,31 @@ class LLGL_EXPORT RenderingProfiler
 
                 using ValueType = unsigned int;
 
+                //! Increment internal counter by one.
                 void Inc()
                 {
                     ++value_;
                 }
 
+                //! Increment internal counter by the specified value.
                 void Inc(ValueType value)
                 {
                     value_ += value;
                 }
 
+                //! Reset internal counter to zero.
                 void Reset()
                 {
                     value_ = 0;
                 }
 
+                //! Returns the internal counter value.
                 inline ValueType Count() const
                 {
                     return value_;
                 }
 
+                //! Returns the internal counter value (same as "Count()" function).
                 inline operator unsigned int () const
                 {
                     return Count();
@@ -74,26 +80,33 @@ class LLGL_EXPORT RenderingProfiler
         void RecordDrawCall(const PrimitiveTopology topology, Counter::ValueType numVertices);
         void RecordDrawCall(const PrimitiveTopology topology, Counter::ValueType numVertices, Counter::ValueType numInstances);
 
-        Counter writeBuffer;
-        Counter mapBuffer;
+        Counter writeBuffer;            //!< Counter for buffer writings. \see RenderSystem::WriteBuffer
+        Counter mapBuffer;              //!< Counter for buffer mappings. \see RenderSystem::MapBuffer
 
-        Counter setVertexBuffer;
-        Counter setIndexBuffer;
-        Counter setConstantBuffer;
-        Counter setStorageBuffer;
-        Counter setGraphicsPipeline;
-        Counter setComputePipeline;
-        Counter setTexture;
-        Counter setSampler;
-        Counter setRenderTarget;
+        Counter setVertexBuffer;        //!< Counter for vertex buffer bindings. \see CommandBuffer::SetVertexBuffer
+        Counter setIndexBuffer;         //!< Counter for index buffer bindings. \see CommandBuffer::SetIndexBuffer
+        Counter setConstantBuffer;      //!< Counter for constant buffer bindings. \see CommandBuffer::SetConstantBuffer
+        Counter setStorageBuffer;       //!< Counter for storage buffer bindings. \see CommandBuffer::SetStorageBuffer
+        Counter setGraphicsPipeline;    //!< Counter for graphics pipeline bindings. \see CommandBuffer::SetGraphicsPipeline
+        Counter setComputePipeline;     //!< Counter for compute pipeline bindings. \see CommandBuffer::SetComputePipeline
+        Counter setTexture;             //!< Counter for texture bindings. \see CommandBuffer::SetTexture
+        Counter setSampler;             //!< Counter for sampler bindings. \see CommandBuffer::SetSampler
+        Counter setRenderTarget;        //!< Counter for render target bindings. \see CommandBuffer::SetRenderTarget
 
+        /**
+        \brief Counter for draw calls.
+        \see CommandBuffer.Draw
+        \see CommandBuffer.DrawIndexed
+        \see CommandBuffer.DrawInstanced
+        \see CommandBuffer.DrawIndexedInstanced
+        */
         Counter drawCalls;
-        Counter dispatchComputeCalls;
+        Counter dispatchComputeCalls;   //!< Counter for dispatch compute calls. \see CommandBuffer::DispatchCompute
 
-        Counter renderedPoints;
-        Counter renderedLines;
-        Counter renderedTriangles;
-        Counter renderedPatches;
+        Counter renderedPoints;         //!< Counter for rendered point primitives.
+        Counter renderedLines;          //!< Counter for rendered line primitives.
+        Counter renderedTriangles;      //!< Counter for rendered triangle primitives.
+        Counter renderedPatches;        //!< Counter for rendered patch primitives.
 
 };
 
