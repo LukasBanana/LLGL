34adc53d385d6f8610e567e9a4338ff576225af9
Lukas Hermanns <lukas.hermanns90@gmail.com>
2016-10-08 00:21:51 +0200

    Updated indent.

--- tutorial/Tutorial01_HelloTriangle/main.cpp
+++ tutorial/Tutorial01_HelloTriangle/main.cpp
@@ -64,9 +64,9 @@ int main(int argc, char* argv[])
         // Create vertex buffer
         LLGL::BufferDescriptor vertexBufferDesc;
         {
-            vertexBufferDesc.type                           = LLGL::BufferType::Vertex;
-            vertexBufferDesc.size                           = sizeof(vertices);             // Size (in bytes) of the vertex buffer
-            vertexBufferDesc.usage                          = LLGL::BufferUsage::Static;    // Buffer usage is static since we won't change it frequently
+            vertexBufferDesc.type                       = LLGL::BufferType::Vertex;
+            vertexBufferDesc.size                       = sizeof(vertices);             // Size (in bytes) of the vertex buffer
+            vertexBufferDesc.usage                      = LLGL::BufferUsage::Static;    // Buffer usage is static since we won't change it frequently
             vertexBufferDesc.vertexBuffer.vertexFormat  = vertexFormat;                 // Vertex format layout
         }
         LLGL::Buffer* vertexBuffer = renderer->CreateBuffer(vertexBufferDesc, vertices);
