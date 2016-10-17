8737c2536967685c02099fedeb495207b059a961
Lukas Hermanns <lukas.hermanns90@gmail.com>
2016-10-17 18:28:05 +0200

    Added extra shader files for Tutorial01 on MacOS port.

---
+++ tutorial/Tutorial01_HelloTriangle/vertex.140core.glsl
@@ -0,0 +1,16 @@
+// GLSL shader version 1.40 core (for OpenGL 3.2 Core Profile)
+#version 140
+
+// Vertex attributes (these names must match our vertex format attributes)
+in vec2 position;
+in vec3 color;
+
+// Vertex output to the fragment shader
+out vec3 vertexColor;
+
+// Vertex shader main function
+void main()
+{
+	gl_Position = vec4(position, 0, 1);
+	vertexColor = color;
+}
