588217d39ddc56584c84d0ea5b39770e5adac9eb
Lukas Hermanns <lukas.hermanns90@gmail.com>
2016-10-19 12:18:59 +0200

    Added internal docu to GLContext class.

--- sources/Renderer/OpenGL/Platform/GLContext.h
+++ sources/Renderer/OpenGL/Platform/GLContext.h
@@ -26,12 +26,19 @@ class GLContext
 
         virtual ~GLContext();
 
+        // Creates a platform specific GLContext instance.
         static std::unique_ptr<GLContext> Create(RenderContextDescriptor& desc, Window& window, GLContext* sharedContext);
 
+        // Makes the specified GLContext current. If null, the current context will be deactivated.
         static bool MakeCurrent(GLContext* context);
+
+        // Returns the active GLContext instance.
         static GLContext* Active();
 
+        // Sets the swap interval for vsync (Win32: wglSwapIntervalEXT, X11: glXSwapIntervalSGI).
         virtual bool SetSwapInterval(int interval) = 0;
+
+        // Swaps the back buffer with the front buffer (Win32: ::SwapBuffers, X11: glXSwapBuffers).
         virtual bool SwapBuffers() = 0;
 
         inline const std::shared_ptr<GLStateManager>& GetStateManager() const
@@ -43,6 +50,7 @@ class GLContext
 
         GLContext(GLContext* sharedContext);
 
+        // Activates or deactivates this GLContext (Win32: wglMakeCurrent, X11: glXMakeCurrent).
         virtual bool Activate(bool activate) = 0;
 
     private:
