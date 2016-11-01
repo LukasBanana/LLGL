e738f4e8f6b6eb163647e6a24aa436c77ef68e29
Lukas Hermanns <lukas.hermanns90@gmail.com>
2016-11-01 18:29:24 +0100

    Removed unused code from GLTexImage files.

--- sources/Renderer/GLCommon/Texture/GLTexImage.h
+++ sources/Renderer/GLCommon/Texture/GLTexImage.h
@@ -9,15 +9,32 @@
 #define LLGL_GL_TEX_IMAGE_H
 
 
-#include "../OpenGL.h"
+#include <LLGL/Image.h>
+#include <LLGL/TextureFlags.h>
 
 
 namespace LLGL
 {
 
+/*
+void GLBuildTexture1D(const TextureDescriptor& desc, const ImageDescriptor* imageDesc);
+void GLBuildTexture2D(const TextureDescriptor& desc, const ImageDescriptor* imageDesc);
+void GLBuildTexture3D(const TextureDescriptor& desc, const ImageDescriptor* imageDesc);
+void GLBuildTextureCube(const TextureDescriptor& desc, const ImageDescriptor* imageDesc);
+void GLBuildTexture1DArray(const TextureDescriptor& desc, const ImageDescriptor* imageDesc);
+void GLBuildTexture2DArray(const TextureDescriptor& desc, const ImageDescriptor* imageDesc);
+void GLBuildTextureCubeArray(const TextureDescriptor& desc, const ImageDescriptor* imageDesc);
+void GLBuildTexture2DMS(const TextureDescriptor& desc);
+void GLBuildTexture2DMSArray(const TextureDescriptor& desc);
 
-
-
+void GLWriteTexture1D(const SubTextureDescriptor& desc, const ImageDescriptor& imageDesc);
+void GLWriteTexture2D(const SubTextureDescriptor& desc, const ImageDescriptor& imageDesc);
+void GLWriteTexture3D(const SubTextureDescriptor& desc, const ImageDescriptor& imageDesc);
+void GLWriteTextureCube(const SubTextureDescriptor& desc, const ImageDescriptor& imageDesc);
+void GLWriteTexture1DArray(const SubTextureDescriptor& desc, const ImageDescriptor& imageDesc);
+void GLWriteTexture2DArray(const SubTextureDescriptor& desc, const ImageDescriptor& imageDesc);
+void GLWriteTextureCubeArray(const SubTextureDescriptor& desc, const ImageDescriptor& imageDesc);
+*/
 
 } // /namespace LLGL
 
