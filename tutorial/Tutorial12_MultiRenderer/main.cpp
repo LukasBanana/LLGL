6475cc5e140d4132a8bf80f6de5d09875df7bff9
Lukas Hermanns <lukas.hermanns90@gmail.com>
2016-12-28 13:08:36 +0100

    Completed Tutorial12.

--- tutorial/Tutorial12_MultiRenderer/main.cpp
+++ tutorial/Tutorial12_MultiRenderer/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char* argv[])
         // Create main window
         LLGL::WindowDescriptor mainWindowDesc;
         {
-            mainWindowDesc.title    = L"LLGL Tutorial 12: Multi Renderer";
+            mainWindowDesc.title    = L"LLGL Tutorial 12: Multi Renderer ( OpenGL and Direct3D 11 )";
             mainWindowDesc.size     = { 800, 600 };
             mainWindowDesc.centered = true;
         }
@@ -55,17 +55,29 @@ int main(int argc, char* argv[])
         }
         std::shared_ptr<LLGL::Window> subWindow0 = LLGL::Window::Create(subWindow0Desc);
 
-        // Load render system module
-        Tutorial::Debugger deb;
-        std::shared_ptr<LLGL::RenderSystem> rendererGL = LLGL::RenderSystem::Load("OpenGL", nullptr, &deb);
+        // Create sub window for 2nd renderer
+        LLGL::WindowDescriptor subWindow1Desc;
+        {
+            subWindow1Desc.position         = { 400,   0 };
+            subWindow1Desc.size             = { 400, 600 };
+            subWindow1Desc.borderless       = true;
+            subWindow1Desc.visible          = true;
+            subWindow1Desc.windowContext    = (&mainWindowContextHandle);
+        }
+        std::shared_ptr<LLGL::Window> subWindow1 = LLGL::Window::Create(subWindow1Desc);
 
-        // Create render context
+        // Load render systems
+        auto rendererGL = LLGL::RenderSystem::Load("OpenGL");
+        auto rendererD3D = LLGL::RenderSystem::Load("Direct3D11");
+
+        // Create render contexts
         LLGL::RenderContextDescriptor contextDesc;
         {
             contextDesc.videoMode.resolution    = { 400, 600 };
             contextDesc.multiSampling           = LLGL::MultiSamplingDescriptor(8);
         }
-        LLGL::RenderContext* contextGL = rendererGL->CreateRenderContext(contextDesc, subWindow0);
+        auto contextGL = rendererGL->CreateRenderContext(contextDesc, subWindow0);
+        auto contextD3D = rendererD3D->CreateRenderContext(contextDesc, subWindow1);
 
         // Show main window
         mainWindow->Show();
@@ -79,15 +91,17 @@ int main(int argc, char* argv[])
         vertexFormat.AppendAttribute({ "position", LLGL::VectorType::Float3 });
         vertexFormat.AppendAttribute({ "texCoord", LLGL::VectorType::Float2 });
 
-        // Create vertex buffer
+        // Create vertex buffers
         const auto vertexBufferDesc = LLGL::VertexBufferDesc(sizeof(Tutorial::VertexPositionTexCoord) * cubeVertices.size(), vertexFormat);
         auto vertexBufferGL = rendererGL->CreateBuffer(vertexBufferDesc, cubeVertices.data());
+        auto vertexBufferD3D = rendererD3D->CreateBuffer(vertexBufferDesc, cubeVertices.data());
 
-        // Create index buffer
+        // Create index buffers
         const auto indexBufferDesc = LLGL::IndexBufferDesc(sizeof(unsigned int) * cubeIndices.size(), LLGL::DataType::UInt32);
         auto indexBufferGL = rendererGL->CreateBuffer(indexBufferDesc, cubeIndices.data());
+        auto indexBufferD3D = rendererD3D->CreateBuffer(indexBufferDesc, cubeIndices.data());
 
-        // Create constant buffer
+        // Create constant buffers
         struct Matrices
         {
             Gs::Matrix4f wvpMatrix;
@@ -96,12 +110,13 @@ int main(int argc, char* argv[])
 
         const auto constBufferDesc = LLGL::ConstantBufferDesc(sizeof(matrices));
         auto constBufferGL = rendererGL->CreateBuffer(constBufferDesc);
+        auto constBufferD3D = rendererD3D->CreateBuffer(constBufferDesc);
 
         auto CreateSceneShader = [&vertexFormat](LLGL::RenderSystem* renderer)
         {
             // Create shaders
-            LLGL::Shader* vertShader = renderer->CreateShader(LLGL::ShaderType::Vertex);
-            LLGL::Shader* fragShader = renderer->CreateShader(LLGL::ShaderType::Fragment);
+            auto vertShader = renderer->CreateShader(LLGL::ShaderType::Vertex);
+            auto fragShader = renderer->CreateShader(LLGL::ShaderType::Fragment);
 
             if (renderer->GetRenderingCaps().shadingLanguage >= LLGL::ShadingLanguage::HLSL_2_0)
             {
@@ -116,7 +131,7 @@ int main(int argc, char* argv[])
             }
 
             // Create shader program which is used as composite
-            LLGL::ShaderProgram* shaderProgram = renderer->CreateShaderProgram();
+            auto shaderProgram = renderer->CreateShaderProgram();
 
             // Attach vertex- and fragment shader to the shader program
             shaderProgram->AttachShader(*vertShader);
@@ -132,20 +147,23 @@ int main(int argc, char* argv[])
             return shaderProgram;
         };
 
-        // Create texture
+        // Create textures
         auto textureGL = Tutorial::LoadTextureWithRenderer(*rendererGL, "../Media/Textures/Logo_OpenGL.png");
+        auto textureD3D = Tutorial::LoadTextureWithRenderer(*rendererD3D, "../Media/Textures/Logo_Direct3D.png");
 
-        // Create sampler
+        // Create samplers
         LLGL::SamplerDescriptor samplerDesc;
         {
             samplerDesc.maxAnisotropy = 8;
         }
         auto samplerGL = rendererGL->CreateSampler(samplerDesc);
+        auto samplerD3D = rendererD3D->CreateSampler(samplerDesc);
 
-        // Create shader program
+        // Create shader programs
         auto shaderProgramGL = CreateSceneShader(rendererGL.get());
+        auto shaderProgramD3D = CreateSceneShader(rendererD3D.get());
 
-        // Create graphics pipeline
+        // Create graphics pipelines
         LLGL::GraphicsPipelineDescriptor pipelineDescGL;
         {
             pipelineDescGL.shaderProgram            = shaderProgramGL;
@@ -153,18 +171,32 @@ int main(int argc, char* argv[])
             pipelineDescGL.depth.writeEnabled       = true;
             pipelineDescGL.rasterizer.multiSampling = contextDesc.multiSampling;
         }
-        LLGL::GraphicsPipeline* pipelineGL = rendererGL->CreateGraphicsPipeline(pipelineDescGL);
+        auto pipelineGL = rendererGL->CreateGraphicsPipeline(pipelineDescGL);
 
-        // Create command buffer to submit subsequent graphics commands to the GPU
-        LLGL::CommandBuffer* commandsGL = rendererGL->CreateCommandBuffer();
+        LLGL::GraphicsPipelineDescriptor pipelineDescD3D;
+        {
+            pipelineDescD3D.shaderProgram               = shaderProgramD3D;
+            pipelineDescD3D.depth.testEnabled           = true;
+            pipelineDescD3D.depth.writeEnabled          = true;
+            pipelineDescD3D.rasterizer.multiSampling    = contextDesc.multiSampling;
+        }
+        auto pipelineD3D = rendererD3D->CreateGraphicsPipeline(pipelineDescD3D);
+
+        // Create command buffers
+        auto commandsGL = rendererGL->CreateCommandBuffer();
+        auto commandsD3D = rendererD3D->CreateCommandBuffer();
         
         // Set the render context as the initial render target
         commandsGL->SetRenderTarget(*contextGL);
+        commandsD3D->SetRenderTarget(*contextD3D);
 
+        // Set background color
         commandsGL->SetClearColor({ 0.1f, 0.1f, 0.4f });
+        commandsD3D->SetClearColor({ 0.1f, 0.1f, 0.4f });
 
-        // Set viewport (this guaranteed to be a persistent state)
+        // Set viewports (this guaranteed to be a persistent state)
         commandsGL->SetViewport({ 0, 0, 800, 600 });
+        commandsD3D->SetViewport({ -400, 0, 800, 600 });
 
         // Initialize matrices
         Gs::Matrix4f projMatrix, viewMatrix, worldMatrix;
@@ -178,25 +210,51 @@ int main(int argc, char* argv[])
             Gs::RotateFree(worldMatrix, Gs::Vector3f(0, 1, 0), Gs::Deg2Rad(0.005f));
             matrices.wvpMatrix = projMatrix * viewMatrix * worldMatrix;
 
-            // Clear color buffer
-            commandsGL->Clear(LLGL::ClearFlags::ColorDepth);
+            // Draw scene for OpenGL
+            {
+                // Clear color buffer
+                commandsGL->Clear(LLGL::ClearFlags::ColorDepth);
 
-            // Update constant buffer
-            rendererGL->WriteBuffer(*constBufferGL, &matrices, sizeof(matrices), 0);
+                // Update constant buffer
+                rendererGL->WriteBuffer(*constBufferGL, &matrices, sizeof(matrices), 0);
 
-            // Set graphics pipeline and vertex buffer
-            commandsGL->SetGraphicsPipeline(*pipelineGL);
-            commandsGL->SetConstantBuffer(*constBufferGL, 0);
-            commandsGL->SetVertexBuffer(*vertexBufferGL);
-            commandsGL->SetIndexBuffer(*indexBufferGL);
-            commandsGL->SetSampler(*samplerGL, 0);
-            commandsGL->SetTexture(*textureGL, 0);
+                // Set graphics pipeline and vertex buffer
+                commandsGL->SetGraphicsPipeline(*pipelineGL);
+                commandsGL->SetConstantBuffer(*constBufferGL, 0);
+                commandsGL->SetVertexBuffer(*vertexBufferGL);
+                commandsGL->SetIndexBuffer(*indexBufferGL);
+                commandsGL->SetSampler(*samplerGL, 0);
+                commandsGL->SetTexture(*textureGL, 0);
 
-            // Draw triangulated cube
-            commandsGL->DrawIndexed(36, 0);
+                // Draw triangulated cube
+                commandsGL->DrawIndexed(36, 0);
 
-            // Present the result on the screen
-            contextGL->Present();
+                // Present the result on the screen
+                contextGL->Present();
+            }
+
+            // Draw scene for Direct3D
+            {
+                // Clear color buffer
+                commandsD3D->Clear(LLGL::ClearFlags::ColorDepth);
+
+                // Update constant buffer
+                rendererD3D->WriteBuffer(*constBufferD3D, &matrices, sizeof(matrices), 0);
+
+                // Set graphics pipeline and vertex buffer
+                commandsD3D->SetGraphicsPipeline(*pipelineD3D);
+                commandsD3D->SetConstantBuffer(*constBufferD3D, 0);
+                commandsD3D->SetVertexBuffer(*vertexBufferD3D);
+                commandsD3D->SetIndexBuffer(*indexBufferD3D);
+                commandsD3D->SetSampler(*samplerD3D, 0);
+                commandsD3D->SetTexture(*textureD3D, 0);
+
+                // Draw triangulated cube
+                commandsD3D->DrawIndexed(36, 0);
+
+                // Present the result on the screen
+                contextD3D->Present();
+            }
         }
     }
     catch (const std::exception& e)
