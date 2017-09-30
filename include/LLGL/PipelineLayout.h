9cd5db0a8e0b60604775670c32f9564f1ef3b4e0
Lukas Hermanns <lukas.hermanns90@gmail.com>
2017-09-30 19:18:05 +0200

    Started with PipelineLayout interface (for Vulkan and D3D12 resource bindings).

---
+++ include/LLGL/PipelineLayout.h
@@ -0,0 +1,44 @@
+/*
+ * PipelineLayout.h
+ * 
+ * This file is part of the "LLGL" project (Copyright (c) 2015-2017 by Lukas Hermanns)
+ * See "LICENSE.txt" for license information.
+ */
+
+#ifndef LLGL_PIPELINE_LAYOUT_H
+#define LLGL_PIPELINE_LAYOUT_H
+
+
+#include "Export.h"
+#include "PipelineLayoutFlags.h"
+
+
+namespace LLGL
+{
+
+
+/**
+\brief Pipeline layout interface.
+\remarks An instance of this interface provides all descriptor sets (as called in Vulkan)
+or descriptor heaps (as called in Direct3D 12) for graphics and compute pipelines.
+*/
+class LLGL_EXPORT PipelineLayout
+{
+
+    public:
+
+        virtual ~PipelineLayout()
+        {
+        }
+
+};
+
+
+} // /namespace LLGL
+
+
+#endif
+
+
+
+// ================================================================================
---
+++ include/LLGL/PipelineLayoutFlags.h
@@ -0,0 +1,71 @@
+/*
+ * PipelineLayoutFlags.h
+ * 
+ * This file is part of the "LLGL" project (Copyright (c) 2015-2017 by Lukas Hermanns)
+ * See "LICENSE.txt" for license information.
+ */
+
+#ifndef LLGL_PIPELINE_LAYOUT_FLAGS_H
+#define LLGL_PIPELINE_LAYOUT_FLAGS_H
+
+
+#include "Export.h"
+#include "BufferFlags.h"
+#include "ShaderFlags.h"
+#include <vector>
+
+
+namespace LLGL
+{
+
+
+/* ----- Enumerations ----- */
+
+/**
+\brief Layout binding type enumeration.
+\see LayoutBinding::type
+*/
+enum class LayoutBindingType
+{
+    Sampler,        //!< Sampler state resource.
+    Texture,        //!< Texture (or image) resource.
+    ConstantBuffer, //!< Constant buffer (or uniform buffer) resource.
+    StorageBuffer,  //!< Storage buffer resource.
+};
+
+
+/* ----- Structures ----- */
+
+struct LayoutBinding
+{
+    //! Buffer type for this layout binding. By default LayoutBindingType::ConstantBuffer.
+    LayoutBindingType   type        = LayoutBindingType::ConstantBuffer;
+
+    /**
+    \brief Zero-based binding slot. By default 0.
+    \note For Vulkan, each binding slot of all layout bindings must have a different value within a pipeline layout.
+    */
+    std::uint32_t       slot        = 0;
+
+    //! Specifies which shader stages are affected by this layout binding. By default all shader stages are affected.
+    long                stageFlags  = ShaderStageFlags::AllStages;
+};
+
+/**
+\brief Pipeline layout descritpor structure.
+\remarks Contains all layout bindings that will be used by graphics and compute pipelines.
+*/
+struct PipelineLayoutDescriptor
+{
+    std::vector<LayoutBinding> bindings; //!< List of layout resource bindings.
+};
+
+
+} // /namespace LLGL
+
+
+#endif
+
+
+
+// ================================================================================
