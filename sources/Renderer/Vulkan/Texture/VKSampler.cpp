a9c558bd847fe4bfc36495a23795a5bad095f526
Lukas Hermanns <lukas.hermanns90@gmail.com>
2018-04-13 21:55:44 +0200

    Minor changes in VKSampler class.

--- sources/Renderer/Vulkan/Texture/VKSampler.cpp
+++ sources/Renderer/Vulkan/Texture/VKSampler.cpp
@@ -14,17 +14,17 @@ namespace LLGL
 {
 
 
-static VkFilter GetMinMagFilter(const TextureFilter filter)
+static VkFilter GetVkFilter(const TextureFilter filter)
 {
     return (filter == TextureFilter::Linear ? VK_FILTER_LINEAR : VK_FILTER_NEAREST);
 }
 
-static VkSamplerMipmapMode GetMipMapMode(const TextureFilter filter)
+static VkSamplerMipmapMode GetVkSamplerMipmapMode(const TextureFilter filter)
 {
     return (filter == TextureFilter::Linear ? VK_SAMPLER_MIPMAP_MODE_LINEAR : VK_SAMPLER_MIPMAP_MODE_NEAREST);
 }
 
-static VkBorderColor GetBorderColor(const ColorRGBAf& color)
+static VkBorderColor GetVkBorderColor(const ColorRGBAf& color)
 {
     if (color.a > 0.5f)
     {
@@ -45,9 +45,9 @@ VKSampler::VKSampler(const VKPtr<VkDevice>& device, const SamplerDescriptor& des
         createInfo.sType                    = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
         createInfo.pNext                    = nullptr;
         createInfo.flags                    = 0;
-        createInfo.magFilter                = GetMinMagFilter(desc.magFilter);
-        createInfo.minFilter                = GetMinMagFilter(desc.minFilter);
-        createInfo.mipmapMode               = GetMipMapMode(desc.mipMapFilter);
+        createInfo.magFilter                = GetVkFilter(desc.magFilter);
+        createInfo.minFilter                = GetVkFilter(desc.minFilter);
+        createInfo.mipmapMode               = GetVkSamplerMipmapMode(desc.mipMapFilter);
         createInfo.addressModeU             = VKTypes::Map(desc.textureWrapU);
         createInfo.addressModeV             = VKTypes::Map(desc.textureWrapV);
         createInfo.addressModeW             = VKTypes::Map(desc.textureWrapW);
@@ -58,7 +58,7 @@ VKSampler::VKSampler(const VKPtr<VkDevice>& device, const SamplerDescriptor& des
         createInfo.compareOp                = VKTypes::Map(desc.compareOp);
         createInfo.minLod                   = desc.minLOD;
         createInfo.maxLod                   = desc.maxLOD;
-        createInfo.borderColor              = GetBorderColor(desc.borderColor);
+        createInfo.borderColor              = GetVkBorderColor(desc.borderColor);
         createInfo.unnormalizedCoordinates  = VK_FALSE;
     }
     VkResult result = vkCreateSampler(device, &createInfo, nullptr, sampler_.ReleaseAndGetAddressOf());
