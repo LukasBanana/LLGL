a643e8e4b67e931dbdece3407600715f197bb304
Lukas Hermanns <lukas.hermanns90@gmail.com>
2019-09-01 00:08:24 -0400

    Added build and run script for Linux.

---
+++ BuildLinux.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+
+SOURCE_DIR=$PWD
+OUTPUT_DIR="build"
+
+# Check packages are installed
+REQUIRED_PKG=""
+
+dpkg -s "git" 2>&1 >/dev/null
+if [ $? -ne 0 ]; then
+    REQUIRED_PKG="${REQUIRED_PKG} git"
+fi
+
+dpkg -s "cmake" 2>&1 >/dev/null
+if [ $? -ne 0 ]; then
+    REQUIRED_PKG="${REQUIRED_PKG} cmake"
+fi
+
+dpkg -s "libx11-dev" 2>&1 >/dev/null
+if [ $? -ne 0 ]; then
+    REQUIRED_PKG="${REQUIRED_PKG} libx11-dev"
+fi
+
+dpkg -s "libxxf86vm-dev" 2>&1 >/dev/null
+if [ $? -ne 0 ]; then
+    REQUIRED_PKG="${REQUIRED_PKG} libxxf86vm-dev"
+fi
+
+dpkg -s "libxrandr-dev" 2>&1 >/dev/null
+if [ $? -ne 0 ]; then
+    REQUIRED_PKG="${REQUIRED_PKG} libxrandr-dev"
+fi
+
+dpkg -s "mesa-common-dev" 2>&1 >/dev/null
+if [ $? -ne 0 ]; then
+    REQUIRED_PKG="${REQUIRED_PKG} mesa-common-dev"
+fi
+
+dpkg -s "libglu1-mesa-dev" 2>&1 >/dev/null
+if [ $? -ne 0 ]; then
+    REQUIRED_PKG="${REQUIRED_PKG} libglu1-mesa-dev"
+fi
+
+dpkg -s "freeglut3-dev" 2>&1 >/dev/null
+if [ $? -ne 0 ]; then
+    REQUIRED_PKG="${REQUIRED_PKG} freeglut3-dev"
+fi
+
+if [ ! -z "$REQUIRED_PKG" ]; then
+    echo "missing packages! enter the following command to install then:"
+    echo "sudo apt install${REQUIRED_PKG}"
+    exit 0
+fi
+
+# Make output build folder
+if [ ! -f "CMakeLists.txt" ]; then
+    echo "error: file not found: CMakeLists.txt"
+    exit 1
+fi
+
+if [ "$#" -eq 1 ]; then
+    OUTPUT_DIR=$1
+else
+    if [ ! "$#" -eq 0 ]; then
+        echo "error: too many arguemnts"
+        echo "usage: BuildLinux.sh [OUTPUT_DIR]"
+        exit 1
+    fi
+fi
+
+if [ ! -d $OUTPUT_DIR ]; then
+    mkdir $OUTPUT_DIR
+fi
+
+cd $OUTPUT_DIR
+
+# Checkout external depenencies
+GAUSSIAN_LIB_DIR="$OUTPUT_DIR/GaussianLib/include"
+
+if [ ! -d $GAUSSIAN_LIB_DIR ]; then
+    git clone https://github.com/LukasBanana/GaussianLib.git
+fi
+
+# Build into output directory
+cmake -DLLGL_BUILD_RENDERER_OPENGL=ON -DLLGL_BUILD_RENDERER_VULKAN=OFF -DLLGL_BUILD_EXAMPLES=ON -DGaussLib_INCLUDE_DIR:STRING=$GAUSSIAN_LIB_DIR -S $SOURCE_DIR
+cmake --build .
+
---
+++ RunExamplesLinux.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+BUILD_DIR="build"
+
+if [ "$#" -eq 1 ]; then
+    BUILD_DIR=$1
+elif [ -d "build/Example_HelloTriangle.app" ]; then
+    BUILD_DIR="bulid"
+elif [ -d "bin/x64/Linux/Example_HelloTriangle.app" ]; then
+    BUILD_DIR="bin/x64/Linux"
+else
+    echo "error: build folder not found: $BUILD_DIR"
+    exit 1
+fi
+
+PS3='select example: '
+options=(
+	"Hello Triangle"
+	"Tessellation"
+	"Texturing"
+	"Queries"
+	"Render Target"
+	"Multi Context"
+	"Buffer Array"
+	"Compute Shader"
+	"Stream Output"
+	"Instancing"
+	"Post Processing"
+	"Shadow Mapping"
+	"Animation"
+	"Stencil Buffer"
+	"Volume Rendering"
+)
+select opt in "${options[@]}"
+do
+	case $opt in
+	"${options[0]}")
+		(cd examples/Cpp/HelloTriangle; ../../../$BUILD_DIR/Example_HelloTriangle)
+		;;
+	"${options[1]}")
+		(cd examples/Cpp/Tessellation; ../../../$BUILD_DIR/Example_Tessellation)
+		;;
+	"${options[2]}")
+		(cd examples/Cpp/Texturing; ../../../$BUILD_DIR/Example_Texturing)
+		;;
+	"${options[3]}")
+		(cd examples/Cpp/Queries; ../../../$BUILD_DIR/Example_Queries)
+		;;
+	"${options[4]}")
+		(cd examples/Cpp/RenderTarget; ../../../$BUILD_DIR/Example_RenderTarget)
+		;;
+	"${options[5]}")
+		(cd examples/Cpp/MultiContext; ../../../$BUILD_DIR/Example_MultiContext)
+		;;
+	"${options[6]}")
+		(cd examples/Cpp/BufferArray; ../../../$BUILD_DIR/Example_BufferArray)
+		;;
+	"${options[7]}")
+		(cd examples/Cpp/ComputeShader; ../../../$BUILD_DIR/Example_ComputeShader)
+		;;
+	"${options[8]}")
+		(cd examples/Cpp/StreamOutput; ../../../$BUILD_DIR/Example_StreamOutput)
+		;;
+	"${options[9]}")
+		(cd examples/Cpp/Instancing; ../../../$BUILD_DIR/Example_Instancing)
+		;;
+	"${options[10]}")
+		(cd examples/Cpp/PostProcessing; ../../../$BUILD_DIR/Example_PostProcessing)
+		;;
+	"${options[11]}")
+		(cd examples/Cpp/ShadowMapping; ../../../$BUILD_DIR/Example_ShadowMapping)
+		;;
+	"${options[12]}")
+		(cd examples/Cpp/Animation; ../../../$BUILD_DIR/Example_Animation)
+		;;
+	"${options[13]}")
+		(cd examples/Cpp/StencilBuffer; ../../../$BUILD_DIR/Example_StencilBuffer)
+		;;
+	"${options[14]}")
+		(cd examples/Cpp/VolumeRendering; ../../../$BUILD_DIR/Example_VolumeRendering)
+		;;
+	*)
+		echo "invalid selection";;
+	esac
+done
