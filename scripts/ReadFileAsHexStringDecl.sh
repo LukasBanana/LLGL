b8b579441a14571ff073bc42c43623d275b13f7f
Laura Hermanns <lukas.hermanns90@gmail.com>
2023-01-04 00:20:01 -0500

    Added secondary script to generate hex string from binary file.

---
+++ scripts/ReadFileAsHexStringDecl.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ $# -ne 1 ] && [ $# -ne 2 ]; then
+    echo "usage: ReadFileAsHexStringDecl.sh INPUT [DECL]"
+    exit 1
+fi
+
+INPUT=$1
+DECL=$(basename $1)
+if [ $# -eq 2 ]; then
+    DECL=$2
+fi
+
+SCRIPT_PATH="$(dirname $0)/ReadFileAsHexString.py"
+echo "static const char* ${DECL} =" | sed -e "s/\./_/"
+python3 "$SCRIPT_PATH" -spaces 4 -offsets cxx $1
+echo ";"
+echo -n "static const std::size_t ${DECL}_Len = " | sed -e "s/\./_/"
+STR_LEN_LINE=$(python3 "$SCRIPT_PATH" -len -paren $1)
+echo "${STR_LEN_LINE};"
