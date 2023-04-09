1fa9fdb793260e7726486e5b69257590e824b7da
Laura Hermanns <lukas.hermanns90@gmail.com>
2023-04-09 17:35:04 -0400

    Fixed Timer::Tick() on Win32 for first invocation.

--- sources/Platform/Win32/Win32Timer.cpp
+++ sources/Platform/Win32/Win32Timer.cpp
@@ -1,6 +1,6 @@
 /*
  * Win32Timer.cpp
- * 
+ *
  * Copyright (c) 2015 Lukas Hermanns. All rights reserved.
  * Licensed under the terms of the BSD 3-Clause license (see LICENSE.txt).
  */
@@ -19,21 +19,22 @@ namespace Timer
 {
 
 
-static std::atomic<DWORD> g_lastLowResTick;
-static std::atomic<LONGLONG> g_lastHighResTick;
-static std::atomic<LONGLONG> g_lastHighResElapsedTime;
-
 /*
 Specifies whether to enable the adjustment for unexpected leaps in the Win32 performance counter.
 This is caused by unexpected data across the PCI to ISA bridge, aka south bridge. See Microsoft KB274323.
 */
 #define LLGL_LEAP_FORWARD_ADJUSTMENT
 
-LLGL_EXPORT std::uint64_t Frequency()
+static LONGLONG GetPerformanceFrequencyQuadPart()
 {
     LARGE_INTEGER frequency;
     QueryPerformanceFrequency(&frequency);
-    return static_cast<std::uint64_t>(frequency.QuadPart);
+    return frequency.QuadPart;
+}
+
+LLGL_EXPORT std::uint64_t Frequency()
+{
+    return static_cast<std::uint64_t>(GetPerformanceFrequencyQuadPart());
 }
 
 LLGL_EXPORT std::uint64_t Tick()
@@ -43,38 +44,41 @@ LLGL_EXPORT std::uint64_t Tick()
 
     #ifdef LLGL_LEAP_FORWARD_ADJUSTMENT
 
-    /* Check for unexpected leaps */
-    LARGE_INTEGER frequency;
-    QueryPerformanceFrequency(&frequency);
+    static std::atomic<DWORD> lastLowResTick;
+    static std::atomic<LONGLONG> lastHighResTick;
+    static std::atomic<LONGLONG> lastHighResElapsedTime;
 
+    /* Check for unexpected leaps */
     const DWORD     lowResTick          = GetTickCount();
 
-    const DWORD     prevLowResTick      = g_lastLowResTick.exchange(lowResTick);
-    const LONGLONG  prevHighResTick     = g_lastHighResTick.exchange(highResTick.QuadPart);
+    const DWORD     prevLowResTick      = lastLowResTick.exchange(lowResTick);
+    const LONGLONG  prevHighResTick     = lastHighResTick.exchange(highResTick.QuadPart);
+
+    static const LONGLONG frequency = GetPerformanceFrequencyQuadPart();
 
     const DWORD     elapsedLowResMS     = (lowResTick - prevLowResTick);
-    LONGLONG        elapsedHighResMS    = (highResTick.QuadPart - prevHighResTick) * 1000 / frequency.QuadPart;
+    LONGLONG        elapsedHighResMS    = (highResTick.QuadPart - prevHighResTick) * 1000 / frequency;
 
     const LONGLONG  millisecondsOff     = elapsedHighResMS - elapsedLowResMS;
 
-    if (std::abs(millisecondsOff) > 100)
+    if (std::abs(millisecondsOff) > 100 && prevLowResTick > 0)
     {
         /* Adjust leap by difference */
-        const LONGLONG prevElapsedTime = g_lastHighResElapsedTime.load();
+        const LONGLONG prevElapsedTime = lastHighResElapsedTime.load();
 
         const LONGLONG adjustment = (std::min)(
-            (millisecondsOff * frequency.QuadPart / 1000),
+            (millisecondsOff * frequency / 1000),
             (elapsedHighResMS - prevElapsedTime)
         );
         highResTick.QuadPart -= adjustment;
         elapsedHighResMS -= adjustment;
 
         /* Update last state of timer */
-        g_lastHighResTick.store(highResTick.QuadPart);
+        lastHighResTick.store(highResTick.QuadPart);
     }
 
     /* Store last elapsed time */
-    g_lastHighResElapsedTime.store(elapsedHighResMS);
+    lastHighResElapsedTime.store(elapsedHighResMS);
 
     #endif // /LLGL_LEAP_FORWARD_ADJUSTMENT
 
