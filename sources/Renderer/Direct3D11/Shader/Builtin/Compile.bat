44b25e400ecfa313301cea5a6409a8cdc9ee3ac6
Graham Dianaty <gmdianaty@gmail.com>
2023-08-27 22:24:47 -0400

    Move D3D11's builtin shaders to a bytecode header.

--- sources/Renderer/Direct3D11/Shader/Builtin/Compile.bat
+++ sources/Renderer/Direct3D11/Shader/Builtin/Compile.bat
@@ -1,10 +1,10 @@
 REM Compiles all shaders with fxc
 REM Use VisualStudio command line tools to run this batch script
 
-fxc /D TEXTURE_DIM=1 /T cs_5_0 /E CopyTextureFromBuffer /Fo CopyTextureFromBufferCS.Dim1D.cso CopyTextureFromBuffer.hlsl
-fxc /D TEXTURE_DIM=2 /T cs_5_0 /E CopyTextureFromBuffer /Fo CopyTextureFromBufferCS.Dim2D.cso CopyTextureFromBuffer.hlsl
-fxc /D TEXTURE_DIM=3 /T cs_5_0 /E CopyTextureFromBuffer /Fo CopyTextureFromBufferCS.Dim3D.cso CopyTextureFromBuffer.hlsl
+fxc /D TEXTURE_DIM=1 /T cs_5_0 /E CopyTextureFromBuffer /Fh CopyTextureFromBufferCS.Dim1D.inc /Vn CopyTextureFromBufferCS_Dim1D CopyTextureFromBuffer.hlsl
+fxc /D TEXTURE_DIM=2 /T cs_5_0 /E CopyTextureFromBuffer /Fh CopyTextureFromBufferCS.Dim2D.inc /Vn CopyTextureFromBufferCS_Dim2D CopyTextureFromBuffer.hlsl
+fxc /D TEXTURE_DIM=3 /T cs_5_0 /E CopyTextureFromBuffer /Fh CopyTextureFromBufferCS.Dim3D.inc /Vn CopyTextureFromBufferCS_Dim3D CopyTextureFromBuffer.hlsl
 
-fxc /D TEXTURE_DIM=1 /T cs_5_0 /E CopyBufferFromTexture /Fo CopyBufferFromTextureCS.Dim1D.cso CopyBufferFromTexture.hlsl
-fxc /D TEXTURE_DIM=2 /T cs_5_0 /E CopyBufferFromTexture /Fo CopyBufferFromTextureCS.Dim2D.cso CopyBufferFromTexture.hlsl
-fxc /D TEXTURE_DIM=3 /T cs_5_0 /E CopyBufferFromTexture /Fo CopyBufferFromTextureCS.Dim3D.cso CopyBufferFromTexture.hlsl
+fxc /D TEXTURE_DIM=1 /T cs_5_0 /E CopyBufferFromTexture /Fh CopyBufferFromTextureCS.Dim1D.inc /Vn CopyBufferFromTextureCS_Dim1D CopyBufferFromTexture.hlsl
+fxc /D TEXTURE_DIM=2 /T cs_5_0 /E CopyBufferFromTexture /Fh CopyBufferFromTextureCS.Dim2D.inc /Vn CopyBufferFromTextureCS_Dim2D CopyBufferFromTexture.hlsl
+fxc /D TEXTURE_DIM=3 /T cs_5_0 /E CopyBufferFromTexture /Fh CopyBufferFromTextureCS.Dim3D.inc /Vn CopyBufferFromTextureCS_Dim3D CopyBufferFromTexture.hlsl
