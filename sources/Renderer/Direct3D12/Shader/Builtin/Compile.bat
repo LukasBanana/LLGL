972c80c2e35b47579350b42261f78f93b7ea5b02
Graham Dianaty <gmdianaty@gmail.com>
2023-08-27 23:04:11 -0400

    Move D3D12's builtin shaders to a bytecode header.

--- sources/Renderer/Direct3D12/Shader/Builtin/Compile.bat
+++ sources/Renderer/Direct3D12/Shader/Builtin/Compile.bat
@@ -1,33 +1,33 @@
 REM Compiles all shaders with fxc
 REM Use VisualStudio command line tools to run this batch script
 
-fxc /D NPOT_TEXTURE_CLASS=0 /T cs_5_0 /E GenerateMips1DCS /Fo GenerateMips1DCS.cso GenerateMips1D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=1 /T cs_5_0 /E GenerateMips1DCS /Fo GenerateMips1DCS.OddX.cso GenerateMips1D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=0 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips1DCS /Fo GenerateMips1DCS.sRGB.cso GenerateMips1D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=1 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips1DCS /Fo GenerateMips1DCS.sRGB.OddX.cso GenerateMips1D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=0 /T cs_5_0 /E GenerateMips1DCS /Vn GenerateMips1DCS /Fh GenerateMips1DCS.inc GenerateMips1D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=1 /T cs_5_0 /E GenerateMips1DCS /Vn GenerateMips1DCS_OddX /Fh GenerateMips1DCS.OddX.inc GenerateMips1D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=0 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips1DCS /Vn GenerateMips1DCS_sRGB /Fh GenerateMips1DCS.sRGB.inc GenerateMips1D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=1 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips1DCS /Vn GenerateMips1DCS_sRGB_OddX /Fh GenerateMips1DCS.sRGB.OddX.inc GenerateMips1D.hlsl
 
-fxc /D NPOT_TEXTURE_CLASS=0 /T cs_5_0 /E GenerateMips2DCS /Fo GenerateMips2DCS.cso GenerateMips2D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=1 /T cs_5_0 /E GenerateMips2DCS /Fo GenerateMips2DCS.OddX.cso GenerateMips2D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=2 /T cs_5_0 /E GenerateMips2DCS /Fo GenerateMips2DCS.OddY.cso GenerateMips2D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=3 /T cs_5_0 /E GenerateMips2DCS /Fo GenerateMips2DCS.OddXY.cso GenerateMips2D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=0 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips2DCS /Fo GenerateMips2DCS.sRGB.cso GenerateMips2D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=1 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips2DCS /Fo GenerateMips2DCS.sRGB.OddX.cso GenerateMips2D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=2 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips2DCS /Fo GenerateMips2DCS.sRGB.OddY.cso GenerateMips2D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=3 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips2DCS /Fo GenerateMips2DCS.sRGB.OddXY.cso GenerateMips2D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=0 /T cs_5_0 /E GenerateMips2DCS /Vn GenerateMips2DCS /Fh GenerateMips2DCS.inc GenerateMips2D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=1 /T cs_5_0 /E GenerateMips2DCS /Vn GenerateMips2DCS_OddX /Fh GenerateMips2DCS.OddX.inc GenerateMips2D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=2 /T cs_5_0 /E GenerateMips2DCS /Vn GenerateMips2DCS_OddY /Fh GenerateMips2DCS.OddY.inc GenerateMips2D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=3 /T cs_5_0 /E GenerateMips2DCS /Vn GenerateMips2DCS_OddXY /Fh GenerateMips2DCS.OddXY.inc GenerateMips2D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=0 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips2DCS /Vn GenerateMips2DCS_sRGB /Fh GenerateMips2DCS.sRGB.inc GenerateMips2D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=1 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips2DCS /Vn GenerateMips2DCS_sRGB_OddX /Fh GenerateMips2DCS.sRGB.OddX.inc GenerateMips2D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=2 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips2DCS /Vn GenerateMips2DCS_sRGB_OddY /Fh GenerateMips2DCS.sRGB.OddY.inc GenerateMips2D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=3 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips2DCS /Vn GenerateMips2DCS_sRGB_OddXY /Fh GenerateMips2DCS.sRGB.OddXY.inc GenerateMips2D.hlsl
 
-fxc /D NPOT_TEXTURE_CLASS=0 /T cs_5_0 /E GenerateMips3DCS /Fo GenerateMips3DCS.cso GenerateMips3D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=1 /T cs_5_0 /E GenerateMips3DCS /Fo GenerateMips3DCS.OddX.cso GenerateMips3D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=2 /T cs_5_0 /E GenerateMips3DCS /Fo GenerateMips3DCS.OddY.cso GenerateMips3D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=3 /T cs_5_0 /E GenerateMips3DCS /Fo GenerateMips3DCS.OddXY.cso GenerateMips3D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=4 /T cs_5_0 /E GenerateMips3DCS /Fo GenerateMips3DCS.OddZ.cso GenerateMips3D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=5 /T cs_5_0 /E GenerateMips3DCS /Fo GenerateMips3DCS.OddXZ.cso GenerateMips3D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=6 /T cs_5_0 /E GenerateMips3DCS /Fo GenerateMips3DCS.OddYZ.cso GenerateMips3D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=7 /T cs_5_0 /E GenerateMips3DCS /Fo GenerateMips3DCS.OddXYZ.cso GenerateMips3D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=0 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips3DCS /Fo GenerateMips3DCS.sRGB.cso GenerateMips3D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=1 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips3DCS /Fo GenerateMips3DCS.sRGB.OddX.cso GenerateMips3D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=2 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips3DCS /Fo GenerateMips3DCS.sRGB.OddY.cso GenerateMips3D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=3 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips3DCS /Fo GenerateMips3DCS.sRGB.OddXY.cso GenerateMips3D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=4 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips3DCS /Fo GenerateMips3DCS.sRGB.OddZ.cso GenerateMips3D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=5 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips3DCS /Fo GenerateMips3DCS.sRGB.OddXZ.cso GenerateMips3D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=6 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips3DCS /Fo GenerateMips3DCS.sRGB.OddYZ.cso GenerateMips3D.hlsl
-fxc /D NPOT_TEXTURE_CLASS=7 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips3DCS /Fo GenerateMips3DCS.sRGB.OddXYZ.cso GenerateMips3D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=0 /T cs_5_0 /E GenerateMips3DCS /Vn GenerateMips3DCS /Fh GenerateMips3DCS.inc GenerateMips3D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=1 /T cs_5_0 /E GenerateMips3DCS /Vn GenerateMips3DCS_OddX /Fh GenerateMips3DCS.OddX.inc GenerateMips3D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=2 /T cs_5_0 /E GenerateMips3DCS /Vn GenerateMips3DCS_OddY /Fh GenerateMips3DCS.OddY.inc GenerateMips3D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=3 /T cs_5_0 /E GenerateMips3DCS /Vn GenerateMips3DCS_OddXY /Fh GenerateMips3DCS.OddXY.inc GenerateMips3D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=4 /T cs_5_0 /E GenerateMips3DCS /Vn GenerateMips3DCS_OddZ /Fh GenerateMips3DCS.OddZ.inc GenerateMips3D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=5 /T cs_5_0 /E GenerateMips3DCS /Vn GenerateMips3DCS_OddXZ /Fh GenerateMips3DCS.OddXZ.inc GenerateMips3D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=6 /T cs_5_0 /E GenerateMips3DCS /Vn GenerateMips3DCS_OddYZ /Fh GenerateMips3DCS.OddYZ.inc GenerateMips3D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=7 /T cs_5_0 /E GenerateMips3DCS /Vn GenerateMips3DCS_OddXYZ /Fh GenerateMips3DCS.OddXYZ.inc GenerateMips3D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=0 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips3DCS /Vn GenerateMips3DCS_sRGB /Fh GenerateMips3DCS.sRGB.inc GenerateMips3D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=1 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips3DCS /Vn GenerateMips3DCS_sRGB_OddX /Fh GenerateMips3DCS.sRGB.OddX.inc GenerateMips3D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=2 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips3DCS /Vn GenerateMips3DCS_sRGB_OddY /Fh GenerateMips3DCS.sRGB.OddY.inc GenerateMips3D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=3 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips3DCS /Vn GenerateMips3DCS_sRGB_OddXY /Fh GenerateMips3DCS.sRGB.OddXY.inc GenerateMips3D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=4 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips3DCS /Vn GenerateMips3DCS_sRGB_OddZ /Fh GenerateMips3DCS.sRGB.OddZ.inc GenerateMips3D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=5 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips3DCS /Vn GenerateMips3DCS_sRGB_OddXZ /Fh GenerateMips3DCS.sRGB.OddXZ.inc GenerateMips3D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=6 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips3DCS /Vn GenerateMips3DCS_sRGB_OddYZ /Fh GenerateMips3DCS.sRGB.OddYZ.inc GenerateMips3D.hlsl
+fxc /D NPOT_TEXTURE_CLASS=7 /D LINEAR_TO_SRGB=1 /T cs_5_0 /E GenerateMips3DCS /Vn GenerateMips3DCS_sRGB_OddXYZ /Fh GenerateMips3DCS.sRGB.OddXYZ.inc GenerateMips3D.hlsl
--- sources/Renderer/Direct3D12/Shader/Builtin/D3D12Builtin.h
+++ sources/Renderer/Direct3D12/Shader/Builtin/D3D12Builtin.h
@@ -8,41 +8,67 @@
 #ifndef LLGL_D3D12_BUILTIN_H
 #define LLGL_D3D12_BUILTIN_H
 
+#include "GenerateMips1DCS.inc"
+#include "GenerateMips1DCS.OddX.inc"
+#include "GenerateMips1DCS.sRGB.inc"
+#include "GenerateMips1DCS.sRGB.OddX.inc"
 
-#define LLGL_IDR_GENERATEMIPS1D_CS              1201
-#define LLGL_IDR_GENERATEMIPS1D_CS_ODDX         1202
-#define LLGL_IDR_GENERATEMIPS1D_CS_SRGB         1203
-#define LLGL_IDR_GENERATEMIPS1D_CS_SRGB_ODDX    1204
+#include "GenerateMips2DCS.inc"
+#include "GenerateMips2DCS.OddX.inc"
+#include "GenerateMips2DCS.OddY.inc"
+#include "GenerateMips2DCS.OddXY.inc"
+#include "GenerateMips2DCS.sRGB.inc"
+#include "GenerateMips2DCS.sRGB.OddX.inc"
+#include "GenerateMips2DCS.sRGB.OddY.inc"
+#include "GenerateMips2DCS.sRGB.OddXY.inc"
 
-#define LLGL_IDR_GENERATEMIPS2D_CS              1205
-#define LLGL_IDR_GENERATEMIPS2D_CS_ODDX         1206
-#define LLGL_IDR_GENERATEMIPS2D_CS_ODDY         1207
-#define LLGL_IDR_GENERATEMIPS2D_CS_ODDXY        1208
-#define LLGL_IDR_GENERATEMIPS2D_CS_SRGB         1209
-#define LLGL_IDR_GENERATEMIPS2D_CS_SRGB_ODDX    1210
-#define LLGL_IDR_GENERATEMIPS2D_CS_SRGB_ODDY    1211
-#define LLGL_IDR_GENERATEMIPS2D_CS_SRGB_ODDXY   1212
+#include "GenerateMips3DCS.inc"
+#include "GenerateMips3DCS.OddX.inc"
+#include "GenerateMips3DCS.OddY.inc"
+#include "GenerateMips3DCS.OddXY.inc"
+#include "GenerateMips3DCS.OddZ.inc"
+#include "GenerateMips3DCS.OddXZ.inc"
+#include "GenerateMips3DCS.OddYZ.inc"
+#include "GenerateMips3DCS.OddXYZ.inc"
+#include "GenerateMips3DCS.sRGB.inc"
+#include "GenerateMips3DCS.sRGB.OddX.inc"
+#include "GenerateMips3DCS.sRGB.OddY.inc"
+#include "GenerateMips3DCS.sRGB.OddXY.inc"
+#include "GenerateMips3DCS.sRGB.OddZ.inc"
+#include "GenerateMips3DCS.sRGB.OddXZ.inc"
+#include "GenerateMips3DCS.sRGB.OddYZ.inc"
+#include "GenerateMips3DCS.sRGB.OddXYZ.inc"
 
-#define LLGL_IDR_GENERATEMIPS3D_CS              1213
-#define LLGL_IDR_GENERATEMIPS3D_CS_ODDX         1214
-#define LLGL_IDR_GENERATEMIPS3D_CS_ODDY         1215
-#define LLGL_IDR_GENERATEMIPS3D_CS_ODDXY        1216
-#define LLGL_IDR_GENERATEMIPS3D_CS_ODDZ         1217
-#define LLGL_IDR_GENERATEMIPS3D_CS_ODDXZ        1218
-#define LLGL_IDR_GENERATEMIPS3D_CS_ODDYZ        1219
-#define LLGL_IDR_GENERATEMIPS3D_CS_ODDXYZ       1220
-#define LLGL_IDR_GENERATEMIPS3D_CS_SRGB         1221
-#define LLGL_IDR_GENERATEMIPS3D_CS_SRGB_ODDX    1222
-#define LLGL_IDR_GENERATEMIPS3D_CS_SRGB_ODDY    1223
-#define LLGL_IDR_GENERATEMIPS3D_CS_SRGB_ODDXY   1224
-#define LLGL_IDR_GENERATEMIPS3D_CS_SRGB_ODDZ    1225
-#define LLGL_IDR_GENERATEMIPS3D_CS_SRGB_ODDXZ   1226
-#define LLGL_IDR_GENERATEMIPS3D_CS_SRGB_ODDYZ   1227
-#define LLGL_IDR_GENERATEMIPS3D_CS_SRGB_ODDXYZ  1228
+#define LLGL_IDR_GENERATEMIPS1D_CS              GenerateMips1DCS
+#define LLGL_IDR_GENERATEMIPS1D_CS_ODDX         GenerateMips1DCS_OddX
+#define LLGL_IDR_GENERATEMIPS1D_CS_SRGB         GenerateMips1DCS_sRGB
+#define LLGL_IDR_GENERATEMIPS1D_CS_SRGB_ODDX    GenerateMips1DCS_sRGB_OddX
+
+#define LLGL_IDR_GENERATEMIPS2D_CS              GenerateMips2DCS
+#define LLGL_IDR_GENERATEMIPS2D_CS_ODDX         GenerateMips2DCS_OddX
+#define LLGL_IDR_GENERATEMIPS2D_CS_ODDY         GenerateMips2DCS_OddY
+#define LLGL_IDR_GENERATEMIPS2D_CS_ODDXY        GenerateMips2DCS_OddXY
+#define LLGL_IDR_GENERATEMIPS2D_CS_SRGB         GenerateMips2DCS_sRGB
+#define LLGL_IDR_GENERATEMIPS2D_CS_SRGB_ODDX    GenerateMips2DCS_sRGB_OddX
+#define LLGL_IDR_GENERATEMIPS2D_CS_SRGB_ODDY    GenerateMips2DCS_sRGB_OddY
+#define LLGL_IDR_GENERATEMIPS2D_CS_SRGB_ODDXY   GenerateMips2DCS_sRGB_OddXY
+
+#define LLGL_IDR_GENERATEMIPS3D_CS              GenerateMips3DCS
+#define LLGL_IDR_GENERATEMIPS3D_CS_ODDX         GenerateMips3DCS_OddX
+#define LLGL_IDR_GENERATEMIPS3D_CS_ODDY         GenerateMips3DCS_OddY
+#define LLGL_IDR_GENERATEMIPS3D_CS_ODDXY        GenerateMips3DCS_OddXY
+#define LLGL_IDR_GENERATEMIPS3D_CS_ODDZ         GenerateMips3DCS_OddZ
+#define LLGL_IDR_GENERATEMIPS3D_CS_ODDXZ        GenerateMips3DCS_OddXZ
+#define LLGL_IDR_GENERATEMIPS3D_CS_ODDYZ        GenerateMips3DCS_OddYZ
+#define LLGL_IDR_GENERATEMIPS3D_CS_ODDXYZ       GenerateMips3DCS_OddXYZ
+#define LLGL_IDR_GENERATEMIPS3D_CS_SRGB         GenerateMips3DCS_sRGB
+#define LLGL_IDR_GENERATEMIPS3D_CS_SRGB_ODDX    GenerateMips3DCS_sRGB_OddX
+#define LLGL_IDR_GENERATEMIPS3D_CS_SRGB_ODDY    GenerateMips3DCS_sRGB_OddY
+#define LLGL_IDR_GENERATEMIPS3D_CS_SRGB_ODDXY   GenerateMips3DCS_sRGB_OddXY
+#define LLGL_IDR_GENERATEMIPS3D_CS_SRGB_ODDZ    GenerateMips3DCS_sRGB_OddZ
+#define LLGL_IDR_GENERATEMIPS3D_CS_SRGB_ODDXZ   GenerateMips3DCS_sRGB_OddXZ
+#define LLGL_IDR_GENERATEMIPS3D_CS_SRGB_ODDYZ   GenerateMips3DCS_sRGB_OddYZ
+#define LLGL_IDR_GENERATEMIPS3D_CS_SRGB_ODDXYZ  GenerateMips3DCS_sRGB_OddXYZ
 
 
 #endif
-
-
-
-// ================================================================================
