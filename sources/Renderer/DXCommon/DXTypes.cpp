eb553c1acaf026316d4cfb2424dccc16fee5d200
Laura Hermanns <lukas.hermanns90@gmail.com>
2024-02-10 20:31:55 -0500

    Add support for depth texture sampling of R24G8 and R32G8X24 DXGI formats.

--- sources/Renderer/DXCommon/DXTypes.cpp
+++ sources/Renderer/DXCommon/DXTypes.cpp
@@ -396,8 +396,8 @@ DXGI_FORMAT ToDXGIFormatSRV(const DXGI_FORMAT format)
     {
         case DXGI_FORMAT_R16_TYPELESS:      return DXGI_FORMAT_R16_UNORM;
         case DXGI_FORMAT_R32_TYPELESS:      return DXGI_FORMAT_R32_FLOAT;
-        case DXGI_FORMAT_R24G8_TYPELESS:    return DXGI_FORMAT_UNKNOWN;
-        case DXGI_FORMAT_R32G8X24_TYPELESS: return DXGI_FORMAT_UNKNOWN;
+        case DXGI_FORMAT_R24G8_TYPELESS:    return DXGI_FORMAT_R24_UNORM_X8_TYPELESS;
+        case DXGI_FORMAT_R32G8X24_TYPELESS: return DXGI_FORMAT_R32_FLOAT_X8X24_TYPELESS;
         default:                            return format;
     }
 }
@@ -408,8 +408,8 @@ DXGI_FORMAT ToDXGIFormatUAV(const DXGI_FORMAT format)
     {
         case DXGI_FORMAT_R16_TYPELESS:          return DXGI_FORMAT_R16_UNORM;
         case DXGI_FORMAT_R32_TYPELESS:          return DXGI_FORMAT_R32_FLOAT;
-        case DXGI_FORMAT_R24G8_TYPELESS:        return DXGI_FORMAT_UNKNOWN;
-        case DXGI_FORMAT_R32G8X24_TYPELESS:     return DXGI_FORMAT_UNKNOWN;
+        case DXGI_FORMAT_R24G8_TYPELESS:        return DXGI_FORMAT_R24_UNORM_X8_TYPELESS;
+        case DXGI_FORMAT_R32G8X24_TYPELESS:     return DXGI_FORMAT_R32_FLOAT_X8X24_TYPELESS;
         case DXGI_FORMAT_R8G8B8A8_UNORM_SRGB:   return DXGI_FORMAT_R8G8B8A8_UNORM; // UAVs cannot have typed sRGB format
         case DXGI_FORMAT_B8G8R8A8_UNORM_SRGB:   return DXGI_FORMAT_B8G8R8A8_UNORM; // UAVs cannot have typed sRGB format
         default:                                return format;
