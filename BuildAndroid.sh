LukasBanana/LLGL

commit 00caa11382d493657ce6a2e59611d95ced59982a

[GL] Set "maxColorAttachments" to minimum between GL_MAX_DRAW_BUFFERS and GL_MAX_COLOR_ATTACHMENTS in GL backend.
These values *can* be different in some GL implementations.
Also ensure standard layout for ClearValue and AttachmentClear structs as they are used inside some GLCmd* structs.

sources/Renderer/OpenGL/OpenGL.h
@@ -9,22 +9,28 @@
 #define LLGL_OPENGL_H
 
 
-#if defined LLGL_OPENGL
+#if LLGL_OPENGL
 #   include "GLCoreProfile/OpenGLCore.h"
-#elif defined LLGL_OPENGLES3
+#elif LLGL_OPENGLES3
 #   include "GLESProfile/OpenGLES.h"
-#elif defined LLGL_WEBGL
+#elif LLGL_WEBGL
 #   include "WebGLProfile/WebGL.h"
 #else
 #   error Unknown OpenGL backend
 #endif
 
 
-#if (GL_ARB_draw_indirect || GL_ES_VERSION_3_1) && !defined(LLGL_WEBGL)
+#if LLGL_WEBGL
+
+#define LLGL_GLEXT_TRANSFORM_FEEDBACK
+
+#else // LLGL_WEBGL
+
+#if GL_ARB_draw_indirect || GL_ES_VERSION_3_1
 #   define LLGL_GLEXT_DRAW_INDIRECT
 #endif
 
-#if (GL_ARB_draw_elements_base_vertex || GL_ES_VERSION_3_2) && !defined(LLGL_WEBGL)
+#if GL_ARB_draw_elements_base_vertex || GL_ES_VERSION_3_2
 #   define LLGL_GLEXT_DRAW_ELEMENTS_BASE_VERTEX
 #endif
 
@@ -36,11 +42,11 @@
 #   define LLGL_GLEXT_MULTI_DRAW_INDIRECT
 #endif
 
-#if (GL_ARB_compute_shader || GL_ES_VERSION_3_1) && !defined(LLGL_WEBGL)
+#if GL_ARB_compute_shader || GL_ES_VERSION_3_1
 #   define LLGL_GLEXT_COMPUTE_SHADER
 #endif
 
-#if (GL_KHR_debug || GL_ES_VERSION_3_2) && !defined(LLGL_WEBGL)
+#if GL_KHR_debug || GL_ES_VERSION_3_2
 #   define LLGL_GLEXT_DEBUG 1
 #endif
 
@@ -53,27 +59,27 @@
 #   define LLGL_GLEXT_TRANSFORM_FEEDBACK
 #endif
 
-#if (GL_EXT_draw_buffers2 || GL_ES_VERSION_3_2) && !defined(LLGL_WEBGL)
+#if GL_EXT_draw_buffers2 || GL_ES_VERSION_3_2
 #   define LLGL_GLEXT_DRAW_BUFFERS2
 #endif
 
-#if (GL_ARB_draw_buffers_blend || GL_ES_VERSION_3_2) && !defined(LLGL_WEBGL)
+#if GL_ARB_draw_buffers_blend || GL_ES_VERSION_3_2
 #   define LLGL_GLEXT_DRAW_BUFFERS_BLEND
 #endif
 
-#if (GL_ARB_tessellation_shader || GL_ES_VERSION_3_2) && !defined(LLGL_WEBGL)
+#if GL_ARB_tessellation_shader || GL_ES_VERSION_3_2
 #   define LLGL_GLEXT_TESSELLATION_SHADER
 #endif
 
-#if (GL_ARB_shader_storage_buffer_object || GL_ES_VERSION_3_1) && !defined(LLGL_WEBGL)
+#if GL_ARB_shader_storage_buffer_object || GL_ES_VERSION_3_1
 #   define LLGL_GLEXT_SHADER_STORAGE_BUFFER_OBJECT
 #endif
 
-#if (GL_ARB_program_interface_query || GL_ES_VERSION_3_1) && !defined(LLGL_WEBGL)
+#if GL_ARB_program_interface_query || GL_ES_VERSION_3_1
 #   define LLGL_GLEXT_PROGRAM_INTERFACE_QUERY
 #endif
 
-#if (defined(LLGL_OPENGL) || GL_ES_VERSION_3_1) && !defined(LLGL_WEBGL)
+#if LLGL_OPENGL || GL_ES_VERSION_3_1
 #   define LLGL_GLEXT_GET_TEX_LEVEL_PARAMETER
 #endif
 
@@ -85,31 +91,27 @@
 #   define LLGL_SAMPLER_BORDER_COLOR
 #endif
 
-#if (GL_ARB_shader_image_load_store || GL_ES_VERSION_3_1) && !defined(LLGL_WEBGL)
+#if GL_ARB_shader_image_load_store || GL_ES_VERSION_3_1
 #   define LLGL_GLEXT_MEMORY_BARRIERS
 #endif
 
 // At most one of these should be defined to indicate which API
 // we'll be using to implement fixed-index primitive restart.
 #if GL_ES_VERSION_2_0 || GL_VERSION_4_3
-#   define LLGL_PRIMITIVE_RESTART_FIXED_INDEX
+#   define LLGL_PRIMITIVE_RESTART_FIXED_INDEX 1
 #elif GL_VERSION_3_1
-#   define LLGL_PRIMITIVE_RESTART
-#endif
-
-// GL on macOS, GLES on iOS, and WebGL always require a fragment shader in a shader program.
-#if defined __APPLE__ || LLGL_WEBGL
-#   define LLGL_USE_NULL_FRAGMENT_SHADER 1
-#else
-#   define LLGL_USE_NULL_FRAGMENT_SHADER 0
+#   define LLGL_PRIMITIVE_RESTART 1
 #endif
 
 // WebGL does not support independent stencil face values, even though glStencilFuncSeparate() is provided.
 // Since D3D does not support independent stencil faces, WebGL cannot emulate it in ANGLE or other web rendering abstraction layers.
-#if LLGL_WEBGL
-#   define LLGL_SUPPORTS_INDEPENDENT_STENCIL_FACES 0
-#else
-#   define LLGL_SUPPORTS_INDEPENDENT_STENCIL_FACES 1
+#define LLGL_SUPPORTS_INDEPENDENT_STENCIL_FACES 1
+
+#endif // /LLGL_WEBGL
+
+// GL on macOS, GLES on iOS, and WebGL always require a fragment shader in a shader program.
+#if defined __APPLE__ || LLGL_WEBGL
+#   define LLGL_USE_NULL_FRAGMENT_SHADER 1
 #endif
 
 

sources/Core/ImageFlags.cpp
@@ -413,7 +413,7 @@ static void ReadDepthStencilValue(
     else if (srcFormat == ImageFormat::DepthStencil && dataType == DataType::UInt32)
     {
         /* Read D24UNormS8UInt format: Decompress 24-bit float and 8-bit unsigned integer */
-        value.depth   = static_cast<float>(srcBuffer.uint32[idx] & 0xFFFFFF) / static_cast<float>(0xFFFFFF);
+        value.depth   = static_cast<float>(srcBuffer.uint32[idx] & 0x00FFFFFFu) / static_cast<float>(0x00FFFFFFu);
         value.stencil = srcBuffer.uint32[idx] >> 24;
     }
     else if (srcFormat == ImageFormat::Depth && dataType == DataType::Float32)
@@ -450,8 +450,8 @@ static void WriteDepthStencilValue(
     else if (dstFormat == ImageFormat::DepthStencil && dataType == DataType::UInt32)
     {
         /* Write D24UNormS8UInt format: Decompress 24-bit float and 8-bit unsigned integer */
-        const std::uint32_t depth24 = static_cast<std::uint32_t>(value.depth * static_cast<float>(0xFFFFFF));
-        dstBuffer.uint32[idx] = ((value.stencil & 0xFF)) << 24 | (depth24 & 0xFFFFFF);
+        const std::uint32_t depth24 = static_cast<std::uint32_t>(value.depth * static_cast<float>(0x00FFFFFFu));
+        dstBuffer.uint32[idx] = ((value.stencil & 0x000000FFu)) << 24 | (depth24 & 0x00FFFFFFu);
     }
     else if (dstFormat == ImageFormat::Depth && dataType == DataType::Float32)
     {

sources/Renderer/OpenGL/GLCoreProfile/GLCoreProfileCaps.cpp
@@ -223,7 +223,7 @@ static void GLGetFeatureLimits(const RenderingFeatures& features, RenderingLimit
 
     /* Query integral attributes */
     limits.maxTextureArrayLayers            = GLGetUInt(GL_MAX_ARRAY_TEXTURE_LAYERS);
-    limits.maxColorAttachments              = GLGetUInt(GL_MAX_DRAW_BUFFERS);
+    limits.maxColorAttachments              = std::min<std::uint32_t>(GLGetUInt(GL_MAX_DRAW_BUFFERS), GLGetUInt(GL_MAX_COLOR_ATTACHMENTS));
     limits.maxPatchVertices                 = GLGetUInt(GL_MAX_PATCH_VERTICES);
     limits.maxAnisotropy                    = static_cast<std::uint32_t>(GLGetFloat(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT));
 

sources/Renderer/OpenGL/GLESProfile/GLESProfileCaps.cpp
@@ -173,7 +173,7 @@ static void GLGetFeatureLimits(RenderingLimits& limits, GLint version)
 
     /* Query integral attributes */
     limits.maxTextureArrayLayers            = GLGetUInt(GL_MAX_ARRAY_TEXTURE_LAYERS);
-    limits.maxColorAttachments              = GLGetUInt(GL_MAX_DRAW_BUFFERS);
+    limits.maxColorAttachments              = std::min<std::uint32_t>(GLGetUInt(GL_MAX_DRAW_BUFFERS), GLGetUInt(GL_MAX_COLOR_ATTACHMENTS));
     //limits.maxPatchVertices               = ???
     //limits.maxAnisotropy                  = ???
 

sources/Renderer/OpenGL/GLStaticAssertions.cpp
@@ -89,6 +89,10 @@ LLGL_ASSERT_STDLAYOUT_STRUCT( GLCmdBindGL2XSampler );
 LLGL_ASSERT_STDLAYOUT_STRUCT( GLCmdPushDebugGroup );
 //LLGL_ASSERT_STDLAYOUT_STRUCT( GLCmdPopDebugGroup ); // Unused
 
+// Structs used as payload
+LLGL_ASSERT_STDLAYOUT_STRUCT( ClearValue ); // Payload for GLCmdClearAttachmentsWithRenderPass
+LLGL_ASSERT_STDLAYOUT_STRUCT( AttachmentClear ); // Payload for GLCmdClearBuffers
+
 
 } // /namespace LLGL
 

sources/Renderer/OpenGL/RenderState/GLStateManager.cpp
@@ -1853,7 +1853,7 @@ void GLStateManager::ClearAttachmentsWithRenderPass(
     const ClearValue*   clearValues)
 {
     const ClearValue defaultClearValue;
-    auto mask = renderPassGL.GetClearMask();
+    GLbitfield mask = renderPassGL.GetClearMask();
 
     GLFramebufferClearState clearState;
     PrepareRasterizerStateForClear(clearState);

sources/Renderer/OpenGL/WebGLProfile/WebGLProfileCaps.cpp
@@ -173,7 +173,7 @@ static void GLGetFeatureLimits(RenderingLimits& limits, GLint version)
 
     /* Query integral attributes */
     limits.maxTextureArrayLayers            = GLGetUInt(GL_MAX_ARRAY_TEXTURE_LAYERS);
-    limits.maxColorAttachments              = GLGetUInt(GL_MAX_DRAW_BUFFERS);
+    limits.maxColorAttachments              = std::min<std::uint32_t>(GLGetUInt(GL_MAX_DRAW_BUFFERS), GLGetUInt(GL_MAX_COLOR_ATTACHMENTS));
     //limits.maxPatchVertices               = ???
     //limits.maxAnisotropy                  = ???
 

commit 85da1580d05d5cfe4786c7ddd6856f5a6a4df04d

[Examples] Started porting HelloGame example to Android and Wasm.
- Allow bundling assets from project folder alongside shared asset folder:
  This is added in CMakeLists.txt script function "add_project_resource_files()" for iOS,
  BuildAndroid.sh script for Android, and GenerateHTML5Examples.sh script for Wasm.
- Fixed linker errors of static constexpr members in HelloGame example on Android and compiler error of narrowed type casts on Wasm.
- Allow running examples with immediate command buffer via -i/--icontext arguments.
- Re-enabled selected GL extensions for WebGL profile (this likely broke in 00caa113).

BuildAndroid.sh
@@ -168,6 +168,7 @@ BASE_OPTIONS=(
     -DLLGL_GL_ENABLE_OPENGLES=$GLES_VER
     -DLLGL_BUILD_RENDERER_NULL=$ENABLE_NULL
     -DLLGL_BUILD_RENDERER_VULKAN=$ENABLE_VULKAN
+    -DLLGL_VK_ENABLE_SPIRV_REFLECT=$ENABLE_VULKAN
     -DLLGL_BUILD_EXAMPLES=$ENABLE_EXAMPLES
     -DLLGL_BUILD_TESTS=OFF
     -DLLGL_BUILD_STATIC_LIB=$STATIC_LIB
@@ -284,8 +285,16 @@ generate_app_project()
         readarray -t ASSET_FILTERS < <(tr -d '\r' < "$ASSETS_LIST_FILE")
         ASSET_FILES=()
         for FILTER in ${ASSET_FILTERS[@]}; do
+            # Search for patterns in both the shared assets and current project folder
             for FILE in $ASSETS_SOURCE_DIR/$FILTER; do
-                ASSET_FILES+=( "$FILE" )
+                if [ -f "$FILE" ]; then
+                    ASSET_FILES+=( "$FILE" )
+                fi
+            done
+            for FILE in $PROJECT_SOURCE_DIR/$FILTER; do
+                if [ -f "$FILE" ]; then
+                    ASSET_FILES+=( "$FILE" )
+                fi
             done
         done
 

CMakeLists.txt
@@ -151,21 +151,19 @@ function(find_source_files OUTPUT_LIST FILTERS)
 endfunction()
 
 # Adds a (variadic) list of resource files to the output project files.
-# The resource files must be located in the shared assets folder,
+# The resource files must be located in either the shared assets folder,
 # i.e. either under "examples/Shared/Assets/".
 # Example:
 #   add_project_resource_files(MyProjectFiles "Models/ModelAssets-*.obj" "Textures/TextureAsset-1.jpg")
-function(add_project_resource_files OUTPUT_LIST)
+function(add_project_resource_files OUTPUT_LIST SRC_FOLDER)
     # Iterate over all input filenames and expand their paths depending on their file extensions
     set(ResourceFiles "")
     set(InputFilenames "${ARGN}")
     foreach(InputFile ${InputFilenames})
-        if("${InputFile}" MATCHES ".+\\.(png|jpg|dds|tga)")
-            file(GLOB TextureResourceFiles "${SHARED_ASSETS_DIR}/${InputFile}")
-            list(APPEND ResourceFiles "${TextureResourceFiles}")
-        elseif("${InputFile}" MATCHES ".+\\.(obj)")
-            file(GLOB ModelResourceFiles "${SHARED_ASSETS_DIR}/${InputFile}")
-            list(APPEND ResourceFiles "${ModelResourceFiles}")
+        if("${InputFile}" MATCHES ".+\\.(png|jpg|dds|tga|obj|txt)")
+            file(GLOB SharedResourceFiles "${SHARED_ASSETS_DIR}/${InputFile}")
+            file(GLOB ProjectResourceFiles "${SRC_FOLDER}/${InputFile}")
+            list(APPEND ResourceFiles "${SharedResourceFiles}" "${ProjectResourceFiles}")
         endif()
     endforeach()
     
@@ -215,7 +213,7 @@ function(find_project_source_files OUTPUT_LIST SRC_FILES)
             if(NOT "${AssetsListContents}" STREQUAL "")
                 string(REGEX REPLACE ";" "\\\\;" AssetsListContents "${AssetsListContents}")
                 string(REGEX REPLACE "\n" ";" AssetsListContents "${AssetsListContents}")
-                add_project_resource_files(OutputProjectFiles ${AssetsListContents})
+                add_project_resource_files(OutputProjectFiles "${SRC_FILES}" ${AssetsListContents})
             endif()
         endif()
     endif()

examples/Cpp/Animation/Example.hlsl
@@ -15,24 +15,24 @@ cbuffer Settings : register(b1)
 
 struct VIn
 {
-	float3 position : POSITION;
-	float3 normal   : NORMAL;
+    float3 position : POSITION;
+    float3 normal   : NORMAL;
     float2 texCoord : TEXCOORD;
 };
 
 struct VOut
 {
-	float4 position : SV_Position;
+    float4 position : SV_Position;
     float4 worldPos : WORLDPOS;
-	float4 normal   : NORMAL;
+    float4 normal   : NORMAL;
     float2 texCoord : TEXCOORD;
 };
 
 void VS(VIn inp, out VOut outp)
 {
     outp.worldPos   = mul(wMatrix, float4(inp.position, 1));
-	outp.position	= mul(vpMatrix, outp.worldPos);
-	outp.normal		= mul(wMatrix, float4(inp.normal, 0));
+    outp.position	= mul(vpMatrix, outp.worldPos);
+    outp.normal		= mul(wMatrix, float4(inp.normal, 0));
     outp.texCoord   = inp.texCoord;
 }
 

examples/Cpp/ExampleBase/ExampleBase.cpp
@@ -36,8 +36,6 @@ See https://www.gnu.org/software/gnulib/manual/html_node/inttypes_002eh.html
 #   include <emscripten/html5.h>
 #endif
 
-#define IMMEDIATE_SUBMIT_CMDBUFFER 0
-
 
 /*
  * Global helper functions
@@ -343,6 +341,7 @@ struct ExampleConfig
     bool            vsync           = true;
     bool            debugger        = false;
     long            flags           = 0;
+    bool            immediateSubmit = false;
 };
 
 static ExampleConfig g_Config;
@@ -360,6 +359,8 @@ void ExampleBase::ParseProgramArgs(int argc, char* argv[])
         g_Config.vsync = false;
     if (HasArgument("-d", argc, argv) || HasArgument("--debug", argc, argv))
         g_Config.debugger = true;
+    if (HasArgument("-i", argc, argv) || HasArgument("--icontext", argc, argv))
+        g_Config.immediateSubmit = true;
     if (HasArgument("-nvidia", argc, argv))
         g_Config.flags |= LLGL::RenderSystemFlags::PreferNVIDIA;
     if (HasArgument("-amd", argc, argv))
@@ -533,9 +534,8 @@ ExampleBase::ExampleBase(const LLGL::UTF8String& title)
     LLGL::CommandBufferDescriptor cmdBufferDesc;
     {
         cmdBufferDesc.debugName = "Commands";
-        #if IMMEDIATE_SUBMIT_CMDBUFFER
-        cmdBufferDesc.flags     = LLGL::CommandBufferFlags::ImmediateSubmit;
-        #endif
+        if (g_Config.immediateSubmit)
+            cmdBufferDesc.flags = LLGL::CommandBufferFlags::ImmediateSubmit;
     }
     commands = renderer->CreateCommandBuffer(cmdBufferDesc);
 

examples/Cpp/HelloGame/HelloGame.cpp
@@ -629,7 +629,7 @@ private:
             groundShaders.vs = LoadShader({ LLGL::ShaderType::Vertex,   "HelloGame.hlsl", "VSGround",   "vs_5_0" }, { vertexFormat });
             groundShaders.ps = LoadShader({ LLGL::ShaderType::Fragment, "HelloGame.hlsl", "PSGround",   "ps_5_0" });
         }
-        else if (Supported(LLGL::ShadingLanguage::GLSL))
+        else if (Supported(LLGL::ShadingLanguage::GLSL) || Supported(LLGL::ShadingLanguage::ESSL))
         {
             sceneShaders.vs  = LoadShaderAndPatchClippingOrigin({ LLGL::ShaderType::Vertex,   "HelloGame.VSInstance.450core.vert" }, { vertexFormat });
             sceneShaders.ps  = LoadShader                      ({ LLGL::ShaderType::Fragment, "HelloGame.PSInstance.450core.frag" });
@@ -960,9 +960,9 @@ private:
 
             return LLGL::ColorRGBub
             {
-                ((color >> 16) & 0xFF),
-                ((color >>  8) & 0xFF),
-                ((color      ) & 0xFF)
+                static_cast<std::uint8_t>((color >> 16) & 0xFF),
+                static_cast<std::uint8_t>((color >>  8) & 0xFF),
+                static_cast<std::uint8_t>((color      ) & 0xFF)
             };
         };
 
@@ -1675,6 +1675,29 @@ private:
     }
 
 };
+
+// Clang/GCC need these declared here as well prior to C++17
+constexpr float  Example_HelloGame::levelTransitionSpeed   ;
+constexpr float  Example_HelloGame::levelDoneSpeed         ;
+constexpr float  Example_HelloGame::wallPosY               ;
+constexpr int    Example_HelloGame::inputStackSize         ;
+constexpr float  Example_HelloGame::playerColor[3]         ;
+constexpr float  Example_HelloGame::treeColorGradient[2][3];
+constexpr float  Example_HelloGame::treeAnimSpeed          ;
+constexpr float  Example_HelloGame::treeAnimRadius         ;
+constexpr int    Example_HelloGame::shadowMapSize          ;
+constexpr float  Example_HelloGame::timeOfDayChangeSpeed   ;
+
+constexpr float  Example_HelloGame::playerMoveSpeed        ;
+constexpr float  Example_HelloGame::playerFallAcceleration ;
+constexpr float  Example_HelloGame::playerJumpWait         ;
+constexpr float  Example_HelloGame::playerJumpDuration     ;
+constexpr float  Example_HelloGame::playerJumpHeight       ;
+constexpr int    Example_HelloGame::playerJumpBounces      ;
+constexpr float  Example_HelloGame::playerExplodeDuration  ;
+constexpr float  Example_HelloGame::playerDescendRotations ;
+constexpr float  Example_HelloGame::playerDescendHeight    ;
+constexpr float  Example_HelloGame::playerDescendDuration  ;
 
 LLGL_IMPLEMENT_EXAMPLE(Example_HelloGame);
 

scripts/GenerateHTML5Examples.sh
@@ -99,10 +99,19 @@ generate_html5_page()
     ASSETS_LIST_FILE=$(find "$PROJECT_SOURCE_DIR" -type f -name *.assets.txt)
     if [ -f "$ASSETS_LIST_FILE" ]; then
         # Read asset filenames to copy to package output
+        # and remove '\r' characters when reading the *.asset.txt file from WSL
         ASSET_FILES=()
-        for FILTER in $(cat $ASSETS_LIST_FILE); do
+        for FILTER in $(cat $ASSETS_LIST_FILE | tr -d '\r'); do
+            # Search for patterns in both the shared assets and current project folder
             for FILE in $ASSETS_SOURCE_DIR/$FILTER; do
-                ASSET_FILES+=( $(echo "$FILE" | tr -d '\r') ) # Remove '\r' characters when reading .txt file from WSL
+                if [ -f "$FILE" ]; then
+                    ASSET_FILES+=( "$FILE" )
+                fi
+            done
+            for FILE in $PROJECT_SOURCE_DIR/$FILTER; do
+                if [ -f "$FILE" ]; then
+                    ASSET_FILES+=( "$FILE" )
+                fi
             done
         done
 

sources/Renderer/OpenGL/OpenGL.h
@@ -22,7 +22,12 @@
 
 #if LLGL_WEBGL
 
+#define LLGL_GLEXT_DRAW_INSTANCED 1
+#define LLGL_GLEXT_UNIFORM_BUFFER_OBJECT 1
+#define LLGL_GLEXT_SAMPLER_OBJECTS 1
 #define LLGL_GLEXT_TRANSFORM_FEEDBACK 1
+#define LLGL_GLEXT_VERTEX_ARRAY_OBJECT 1
+#define LLGL_GLEXT_FRAMEBUFFER_OBJECT 1
 
 #else // LLGL_WEBGL
 