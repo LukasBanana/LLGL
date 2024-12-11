2ff376bd3c59d937f9b024b0291dd54cddaf56f0
Laura Hermanns <lukas.hermanns90@gmail.com>
2024-12-10 22:45:46 -0500

    [C#] Added frame profiling output to C# ExampleBase.

--- examples/CSharp/ExampleBase/ExampleBase.cs
+++ examples/CSharp/ExampleBase/ExampleBase.cs
@@ -22,9 +22,14 @@ namespace LLGLExamples
 
             public override void OnKeyDown(LLGL.Window sender, LLGL.Key keyCode)
             {
-                if (keyCode == LLGL.Key.Escape)
+                switch (keyCode)
                 {
-                    sender.PostQuit();
+                    case LLGL.Key.Escape:
+                        sender.PostQuit();
+                        break;
+                    case LLGL.Key.F1:
+                        App.TriggerFrameProfiling();
+                        break;
                 }
             }
 
@@ -38,7 +43,7 @@ namespace LLGLExamples
         protected static readonly LLGL.Color bgColor = new LLGL.Color(0.1f, 0.1f, 0.2f);
         protected static readonly LLGL.Extent2D initialResolution = new LLGL.Extent2D(800, 600);
 
-        protected LLGL.RenderingDebugger Debugger { get; private set; } = new LLGL.RenderingDebugger();
+        protected LLGL.RenderingDebugger Debugger { get; private set; }
         protected LLGL.RenderSystem Renderer { get; private set; }
         protected LLGL.CommandBuffer CmdBuffer { get; private set; }
         protected LLGL.SwapChain SwapChain { get; private set; }
@@ -90,10 +95,13 @@ namespace LLGLExamples
             };
 
             string rendererModule = ExampleBase.GetSelectedRendererModule(args);
-            bool useDebugger = HasArgument("-d") || HasArgument("--debug");
+            if (HasArgument("-d") || HasArgument("--debug"))
+            {
+                Debugger = new LLGL.RenderingDebugger();
+            }
 
             var report = new LLGL.Report();
-            Renderer = LLGL.RenderSystem.Load(new LLGL.RenderSystemDescriptor(rendererModule, debugger: useDebugger ? Debugger : null), report);
+            Renderer = LLGL.RenderSystem.Load(new LLGL.RenderSystemDescriptor(rendererModule, debugger: Debugger), report);
             if (Renderer == null)
             {
                 Console.Write(report.ToString());
@@ -103,6 +111,7 @@ namespace LLGLExamples
             var swapChainDesc = new LLGL.SwapChainDescriptor(resolution: initialResolution, samples: 8);
             swapChainDesc.DebugName = "MySwapChain";
             SwapChain = Renderer.CreateSwapChain(swapChainDesc);
+            SwapChain.SetVsyncInterval(1);
 
             ExampleBase.PrintRendererInfo(Renderer.RendererInfo);
 
@@ -123,15 +132,56 @@ namespace LLGLExamples
             // Main loop
             while (LLGL.Surface.ProcessEvents() && !window.HasQuit)
             {
-                //Debugger.TimeRecording = true;
+                if (profileNextFrame)
+                {
+                    // Record frame timings
+                    Debugger.TimeRecording = true;
+                    OnDrawFrame();
+                    SwapChain.Present();
+                    Debugger.TimeRecording = false;
 
-                OnDrawFrame();
-                SwapChain.Present();
-
-                var frameProfile = Debugger.FlushProfile();
+                    // Print frame profile results to console
+                    PrintFrameProfile(Debugger.FlushProfile());
+                    profileNextFrame = false;
+                }
+                else
+                {
+                    OnDrawFrame();
+                    SwapChain.Present();
+                }
+                ++frameNo;
             }
 
             return 0;
+        }
+
+        private bool profileNextFrame = false;
+        private int frameNo = 0;
+        public void TriggerFrameProfiling()
+        {
+            if (Debugger != null)
+            {
+                profileNextFrame = true;
+            }
+            else
+            {
+                Console.WriteLine("Cannot start frame profiling without debugger. Start with -d or --debug option.");
+            }
+        }
+
+        private void PrintFrameProfile(LLGL.FrameProfile frameProfile)
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"FRAME {frameNo}:");
+            Console.WriteLine(String.Format("{0,30} {1,-10} {2,-10}", "COMMAND", "TIMESTAMP", "DURATION"));
+            Console.WriteLine(String.Format("{0,30} {1,-10} {2,-10}", "-------", "---------", "--------"));
+            double ticksToMilliseconds = 1000.0 / (double)LLGL.Timer.Frequency;
+            foreach (LLGL.ProfileTimeRecord record in frameProfile.TimeRecords)
+            {
+                double timestamp = (double)record.CPUTicksStart * ticksToMilliseconds;
+                double duration = (double)(record.CPUTicksEnd - record.CPUTicksStart) * ticksToMilliseconds;
+                Console.WriteLine(String.Format("{0,30} {1,-10} {2,-10}", $"{(string)record.Annotation}()", $"{timestamp} ms", $"{duration} ms"));
+            }
         }
 
         protected abstract void OnInitialize();
