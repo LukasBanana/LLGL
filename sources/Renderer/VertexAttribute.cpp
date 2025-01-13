2e1e67d9fb02e38a79348f30c12ab8886d143beb
roman <carpowroma@gmail.com>
2025-01-13 21:13:57 +0300

    [Misc] Use move semantics where possible

--- sources/Renderer/VertexAttribute.cpp
+++ sources/Renderer/VertexAttribute.cpp
@@ -5,6 +5,7 @@
  * Licensed under the terms of the BSD 3-Clause license (see LICENSE.txt).
  */
 
+#include <utility>
 #include <LLGL/VertexAttribute.h>
 #include "../Core/MacroUtils.h"
 
@@ -14,13 +15,13 @@ namespace LLGL
 
 
 VertexAttribute::VertexAttribute(
-    const StringLiteral&    name,
-    const Format            format,
-    std::uint32_t           location,
-    std::uint32_t           instanceDivisor,
-    const SystemValue       systemValue)
+    StringLiteral       name,
+    const Format        format,
+    std::uint32_t       location,
+    std::uint32_t       instanceDivisor,
+    const SystemValue   systemValue)
 :
-    name            { name            },
+    name            { std::move(name) },
     format          { format          },
     location        { location        },
     systemValue     { systemValue     },
@@ -29,51 +30,51 @@ VertexAttribute::VertexAttribute(
 }
 
 VertexAttribute::VertexAttribute(
-    const StringLiteral&    semanticName,
-    std::uint32_t           semanticIndex,
-    const Format            format,
-    std::uint32_t           location,
-    std::uint32_t           instanceDivisor)
+    StringLiteral   semanticName,
+    std::uint32_t   semanticIndex,
+    const Format    format,
+    std::uint32_t   location,
+    std::uint32_t   instanceDivisor)
 :
-    name            { semanticName    },
-    format          { format          },
-    location        { location        },
-    semanticIndex   { semanticIndex   },
-    instanceDivisor { instanceDivisor }
+    name            { std::move(semanticName) },
+    format          { format                  },
+    location        { location                },
+    semanticIndex   { semanticIndex           },
+    instanceDivisor { instanceDivisor         }
 {
 }
 
 VertexAttribute::VertexAttribute(
-    const StringLiteral&    name,
-    const Format            format,
-    std::uint32_t           location,
-    std::uint32_t           offset,
-    std::uint32_t           stride,
-    std::uint32_t           slot,
-    std::uint32_t           instanceDivisor)
+    StringLiteral    name,
+    const Format     format,
+    std::uint32_t    location,
+    std::uint32_t    offset,
+    std::uint32_t    stride,
+    std::uint32_t    slot,
+    std::uint32_t    instanceDivisor)
 :
-    VertexAttribute { name, 0, format, location, offset, stride, slot, instanceDivisor }
+    VertexAttribute { std::move(name), 0, format, location, offset, stride, slot, instanceDivisor }
 {
 }
 
 VertexAttribute::VertexAttribute(
-    const StringLiteral&    semanticName,
-    std::uint32_t           semanticIndex,
-    const Format            format,
-    std::uint32_t           location,
-    std::uint32_t           offset,
-    std::uint32_t           stride,
-    std::uint32_t           slot,
-    std::uint32_t           instanceDivisor)
+    StringLiteral    semanticName,
+    std::uint32_t    semanticIndex,
+    const Format     format,
+    std::uint32_t    location,
+    std::uint32_t    offset,
+    std::uint32_t    stride,
+    std::uint32_t    slot,
+    std::uint32_t    instanceDivisor)
 :
-    name            { semanticName    },
-    format          { format          },
-    location        { location        },
-    semanticIndex   { semanticIndex   },
-    slot            { slot            },
-    offset          { offset          },
-    stride          { stride          },
-    instanceDivisor { instanceDivisor }
+    name            { std::move(semanticName) },
+    format          { format                  },
+    location        { location                },
+    semanticIndex   { semanticIndex           },
+    slot            { slot                    },
+    offset          { offset                  },
+    stride          { stride                  },
+    instanceDivisor { instanceDivisor         }
 {
 }
 
