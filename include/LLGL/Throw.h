0b508a3e14b85d08f1df506e656c03a045505ad5
roman <carpowroma@gmail.com>
2025-01-24 15:11:15 +0300

    Add the comments at the top and bottom

--- include/LLGL/Throw.h
+++ include/LLGL/Throw.h
@@ -1,3 +1,10 @@
+/*
+ * Throw.h
+ *
+ * Copyright (c) 2015 Lukas Hermanns. All rights reserved.
+ * Licensed under the terms of the BSD 3-Clause license (see LICENSE.txt).
+ */
+
 #ifndef LLGL_THROW_H
 #define LLGL_THROW_H
 
@@ -14,3 +21,7 @@
 #endif
 
 #endif
+
+
+
+// ================================================================================
