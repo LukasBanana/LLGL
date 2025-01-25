5aadef4a71fabd2168bda0261b5ef388a84726ae
st0rmbtw <carpowroma@gmail.com>
2025-01-25 04:22:31 +0300

    Use the LLGL_VERIFY_OR_THROW macro in the other CastTo function

--- include/LLGL/TypeInfo.h
+++ include/LLGL/TypeInfo.h
@@ -98,7 +98,7 @@ const LLGL::Window& myWindow = LLGL::CastTo<LLGL::Window>(mySurface);
 template <typename T>
 inline const T& CastTo(const Interface& obj)
 {
-    LLGL_THROW_IF(!IsInstanceOf<T>(obj), std::bad_cast());
+    LLGL_VERIFY_OR_THROW(IsInstanceOf<T>(obj), std::bad_cast());
 
     return static_cast<const T&>(obj);
 }
