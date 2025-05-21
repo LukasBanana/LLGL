df0817517b05234065ee7d688606ec66a589f91d
st0rmbtw <>
2025-05-21 15:25:46 +0300

    Check if Wayland is enabled

--- sources/Platform/Linux/LinuxWindow.cpp
+++ sources/Platform/Linux/LinuxWindow.cpp
@@ -20,10 +20,19 @@ namespace LLGL
 
 std::unique_ptr<Window> Window::Create(const WindowDescriptor& desc)
 {
+
     if (desc.wayland)
+    {
+        #if LLGL_LINUX_ENABLE_WAYLAND
         return MakeUnique<LinuxWindowWayland>(desc);
+        #else
+        LLGL_TRAP("Wayland window is requested but LLGL was not built with LLGL_LINUX_ENABLE_WAYLAND");
+        #endif
+    }
     else
+    {
         return MakeUnique<LinuxWindowX11>(desc);
+    }
 }
 
 } // /namespace LLGL
