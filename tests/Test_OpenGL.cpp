dd53d14b9f5161f6b9d2b1616b6f607408925cc2
st0rmbtw <>
2025-05-25 20:08:43 +0300

    Undo redundant changes

--- tests/Test_OpenGL.cpp
+++ tests/Test_OpenGL.cpp
@@ -360,7 +360,7 @@ int main()
         #endif
 
         // Main loop
-        while (window->ProcessEvents() && !window->HasQuit() && !input.KeyDown(LLGL::Key::Escape))
+        while (LLGL::Surface::ProcessEvents() && !window->HasQuit() && !input.KeyDown(LLGL::Key::Escape))
         {
             debugger->FlushProfile();
 
