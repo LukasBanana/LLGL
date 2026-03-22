LukasBanana/LLGL

commit 80b18e65797366011e3bc217039af29a76232700

[VK] Create swap-buffers with picked resolution instead of requested resolution (fixes #207).
This fixes an issue where the wrong resolution might be used to re-create the color and depth-stencil buffers for a Vulkan swap-chain.
Also avoid rendering frames in ExampleBase when the resolution is considered non-drawable, e.g. when the window is minimized.

examples/Cpp/ExampleBase/ExampleBase.cpp
@@ -282,24 +282,14 @@ ExampleBase::WindowEventHandler::WindowEventHandler(ExampleBase& app, LLGL::Swap
 
 void ExampleBase::WindowEventHandler::OnResize(LLGL::Window& sender, const LLGL::Extent2D& clientAreaSize)
 {
-    if (clientAreaSize.width >= 4 && clientAreaSize.height >= 4)
-    {
-        const auto& resolution = clientAreaSize;
+    const auto& resolution = clientAreaSize;
 
-        // Update swap buffers
-        swapChain_->ResizeBuffers(resolution);
+    // Update projection matrix
+    auto aspectRatio = static_cast<float>(resolution.width) / static_cast<float>(resolution.height);
+    projection_ = app_.PerspectiveProjection(aspectRatio, 0.1f, 100.0f, Gs::Deg2Rad(45.0f));
 
-        // Update projection matrix
-        auto aspectRatio = static_cast<float>(resolution.width) / static_cast<float>(resolution.height);
-        projection_ = app_.PerspectiveProjection(aspectRatio, 0.1f, 100.0f, Gs::Deg2Rad(45.0f));
-
-        // Notify application about resize event
-        app_.OnResize(resolution);
-
-        // Re-draw frame
-        if (app_.IsLoadingDone())
-            app_.DrawFrame();
-    }
+    // Notify application about resize event
+    app_.Resize(resolution);
 }
 
 void ExampleBase::WindowEventHandler::OnUpdate(LLGL::Window& sender)
@@ -329,15 +319,12 @@ void ExampleBase::CanvasEventHandler::OnDraw(LLGL::Canvas& /*sender*/)
 
 void ExampleBase::CanvasEventHandler::OnResize(LLGL::Canvas& /*sender*/, const LLGL::Extent2D& clientAreaSize)
 {
-    // Update swap buffers
-    swapChain_->ResizeBuffers(clientAreaSize);
-
     // Update projection matrix
     auto aspectRatio = static_cast<float>(clientAreaSize.width) / static_cast<float>(clientAreaSize.height);
     projection_ = app_.PerspectiveProjection(aspectRatio, 0.1f, 100.0f, Gs::Deg2Rad(45.0f));
 
     // Notify application about resize event
-    app_.OnResize(clientAreaSize);
+    app_.Resize(clientAreaSize);
 }
 
 
@@ -446,13 +433,36 @@ void ExampleBase::Run()
 
 void ExampleBase::DrawFrame()
 {
-    // Draw frame in respective example project
-    OnDrawFrame();
+    if (IsDrawable())
+    {
+        // Draw frame in respective example project
+        OnDrawFrame();
 
-    #ifndef LLGL_OS_IOS
-    // Present the result on the screen - cannot be explicitly invoked on mobile platforms
-    swapChain->Present();
-    #endif
+        #ifndef LLGL_OS_IOS
+        // Present the result on the screen - cannot be explicitly invoked on mobile platforms
+        swapChain->Present();
+        #endif
+    }
+}
+
+void ExampleBase::Resize(const LLGL::Extent2D& clientAreaSize)
+{
+    drawableSize_ = clientAreaSize;
+
+    if (IsDrawable())
+    {
+        // Update swap buffers
+        swapChain->ResizeBuffers(drawableSize_);
+
+        // Re-draw frame
+        if (IsLoadingDone())
+            DrawFrame();
+    }
+}
+
+bool ExampleBase::IsDrawable() const
+{
+    return (drawableSize_.width >= 4 && drawableSize_.height >= 4);
 }
 
 static LLGL::Extent2D ScaleResolution(const LLGL::Extent2D& res, float scale)
@@ -543,7 +553,8 @@ ExampleBase::ExampleBase(const LLGL::UTF8String& title)
 
     swapChain->SetVsyncInterval(g_Config.vsync ? 1 : 0);
 
-    samples_ = swapChain->GetSamples();
+    samples_        = swapChain->GetSamples();
+    drawableSize_   = swapChain->GetResolution();
 
     // Create command buffer
     LLGL::CommandBufferDescriptor cmdBufferDesc;

examples/Cpp/ExampleBase/ExampleBase.h
@@ -85,6 +85,12 @@ public:
     // Draws a frame and presents the result on the screen.
     void DrawFrame();
 
+    // Resizes the swap-chain and notifies the app about the resize.
+    void Resize(const LLGL::Extent2D& clientAreaSize);
+
+    // Returns true if the app can currently draw a frame. Otherwise, the window is resized too small for instance.
+    bool IsDrawable() const;
+
 protected:
 
     struct ShaderDescWrapper
@@ -160,6 +166,8 @@ private:
     LLGL::Extent2D                              initialResolution_;
     bool                                        showTimeRecords_    = false;
     bool                                        fullscreen_         = false;
+
+    LLGL::Extent2D                              drawableSize_;
 
 protected:
 

sources/Renderer/Vulkan/VKSwapChain.cpp
@@ -634,15 +634,17 @@ void VKSwapChain::CreateSwapChainFramebuffers()
     }
 }
 
-void VKSwapChain::CreateDepthStencilBuffer(const Extent2D& resolution)
+void VKSwapChain::CreateDepthStencilBuffer()
 {
+    const Extent2D resolution{ swapChainExtent_.width, swapChainExtent_.height };
     const VkSampleCountFlagBits sampleCountBits = VKTypes::ToVkSampleCountBits(swapChainSamples_);
     depthStencilBuffer_.Create(deviceMemoryMngr_, resolution, depthStencilFormat_, sampleCountBits);
 }
 
-void VKSwapChain::CreateColorBuffers(const Extent2D& resolution)
+void VKSwapChain::CreateColorBuffers()
 {
     /* Create VkImage objects for each swap-chain buffer */
+    const Extent2D& resolution{ swapChainExtent_.width, swapChainExtent_.height };
     const VkSampleCountFlagBits sampleCountBits = VKTypes::ToVkSampleCountBits(swapChainSamples_);
     colorBuffers_.resize(numColorBuffers_);
     for_range(i, numColorBuffers_)
@@ -668,10 +670,10 @@ void VKSwapChain::CreateResolutionDependentResources(const Extent2D& resolution)
     CreateSwapChain(resolution, vsyncInterval_);
 
     if (HasMultiSampling())
-        CreateColorBuffers(resolution);
+        CreateColorBuffers();
 
     if (depthStencilFormat_ != VK_FORMAT_UNDEFINED)
-        CreateDepthStencilBuffer(resolution);
+        CreateDepthStencilBuffer();
 
     CreateSwapChainFramebuffers();
 }

sources/Renderer/Vulkan/VKSwapChain.h
@@ -108,8 +108,8 @@ class VKSwapChain final : public SwapChain
         void CreateSwapChainImageViews();
         void CreateSwapChainFramebuffers();
 
-        void CreateDepthStencilBuffer(const Extent2D& resolution);
-        void CreateColorBuffers(const Extent2D& resolution);
+        void CreateDepthStencilBuffer();
+        void CreateColorBuffers();
         void ReleaseRenderBuffers();
 
         void CreateResolutionDependentResources(const Extent2D& resolution);

commit 6a04ae34180fadeffa227d95c4fe0f8a513fb572

[Examples] Fixed missing call to OnResize() in ExampleBase.
This bug was introduced in 80b18e65.

examples/Cpp/ExampleBase/ExampleBase.cpp
@@ -459,7 +459,10 @@ void ExampleBase::Resize(const LLGL::Extent2D& clientAreaSize)
 
         // Re-draw frame
         if (IsLoadingDone())
+        {
+            OnResize(drawableSize_);
             DrawFrame();
+        }
     }
 }
 