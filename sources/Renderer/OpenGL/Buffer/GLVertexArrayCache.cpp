LukasBanana/LLGL

commit 8c8cf40ec07e94f31c9abfa3cc05f30ea13e98f2

[GL] Started implementing new vertex input attribute API in OpenGL backend.
This adds the GLVertexArrayCache class, which handles caching VAOs from two inputs to separate buffer and attribute inputs.
The command buffer now binds a VAO right before every draw call if it has been invalidated and the cache creates new VAOs on demand.
`GLBufferInputLayout` stores the hash over all input buffers, analogous to `GLVertexInputLayout`.

sources/Renderer/OpenGL/Buffer/GLVertexArrayCache.cpp
@@ -1,0 +1,115 @@
+/*
+ * GLVertexArrayCache.cpp
+ *
+ * Copyright (c) 2015 Lukas Hermanns. All rights reserved.
+ * Licensed under the terms of the BSD 3-Clause license (see LICENSE.txt).
+ */
+
+#include "GLVertexArrayCache.h"
+#include "GLBuffer.h"
+#include "../../../Core/CoreUtils.h"
+#include "../../../Core/MacroUtils.h"
+#include <LLGL/Utils/ForRange.h>
+#include <algorithm>
+
+
+namespace LLGL
+{
+
+
+GLVertexArrayCache& GLVertexArrayCache::Get()
+{
+    static GLVertexArrayCache instance;
+    return instance;
+}
+
+void GLVertexArrayCache::Clear()
+{
+#if 0//TODO: this needs to call `GLVertexArrayObject::Release()`, but they are GL context dependent, so this needs a deferred deletion mechanism
+    for (VertexBufferBinding& vertexBinding : vertexBindings_)
+    {
+        vertexBinding.vertexArray->Reset();
+    }
+#endif
+    vertexBindings_.clear();
+}
+
+GLSharedContextVertexArray* GLVertexArrayCache::GetVertexArray(const GLVertexInputLayout& vertexInputLayout, const GLBufferInputLayout& bufferInputLayout)
+{
+    const ArrayView<GLVertexAttribute>  attribs = vertexInputLayout.GetAttribs();
+    const ArrayView<GLBuffer*>          buffers = bufferInputLayout.GetBuffers();
+
+    if (attribs.empty() || vertexInputLayout.GetHash() == 0 ||
+        buffers.empty() || bufferInputLayout.GetHash() == 0)
+    {
+        return nullptr;
+    }
+
+    /* Get combination of vertex input and buffer input hashes */
+    std::size_t combinedHash = 0;
+    HashCombine(combinedHash, vertexInputLayout.GetHash());
+    HashCombine(combinedHash, bufferInputLayout.GetHash());
+
+    /* Try to find existing vertex buffer binding for input combination */
+    std::size_t insertPosition = 0;
+    VertexBufferBinding* vertexBinding = FindInSortedArray<VertexBufferBinding>(
+        vertexBindings_.data(),
+        vertexBindings_.size(),
+        [combinedHash](const VertexBufferBinding& entry) -> int
+        {
+            LLGL_COMPARE_SEPARATE_MEMBERS_SWO(combinedHash, entry.combinedInputHash);
+            return 0;
+        },
+        &insertPosition
+    );
+
+    if (vertexBinding != nullptr)
+        return vertexBinding->vertexArray.get();
+
+    /* No combination found -> create a new vertex array */
+    GLSharedContextVertexArrayPtr newVertexArray = MakeUnique<GLSharedContextVertexArray>();
+
+    /* Resolve buffer ID for all vertex attributes */
+    std::vector<GLVertexAttribute> vertexAttribs(attribs.begin(), attribs.end());
+    for_range(i, vertexAttribs.size())
+    {
+        const std::uint32_t bufferSlot = vertexAttribs[i].buffer;
+        LLGL_ASSERT(
+            bufferSlot < buffers.size(),
+            "GLVertexAttribute::buffer=%u exceeded upper bound of %zu buffers",
+            i, buffers.size()
+        );
+        vertexAttribs[i].buffer = buffers[bufferSlot]->GetID();
+    }
+
+    /* Build vertex layout and finalize immediately as it only references a single buffer */
+    newVertexArray->BuildVertexLayout(vertexAttribs);
+    newVertexArray->Finalize();
+
+    /* Cache new vertex array in sorted list */
+    auto itNewEntry = vertexBindings_.insert(
+        vertexBindings_.begin() + insertPosition,
+        VertexBufferBinding{ combinedHash, SmallVector<GLBuffer*, 1>{ buffers.begin(), buffers.end() }, std::move(newVertexArray) }
+    );
+    return itNewEntry->vertexArray.get();
+}
+
+//TODO: this could use some speedup as it is currently running with O(n) complexity
+void GLVertexArrayCache::NotifyBufferRelease(const GLBuffer& buffer)
+{
+    /* Run through all vertex buffer bindings to see which one must be destroyed */
+    RemoveAllFromListIf(
+        vertexBindings_,
+        [bufferPtr = &buffer](const VertexBufferBinding& entry) -> bool
+        {
+            return (std::find(entry.buffers.begin(), entry.buffers.end(), bufferPtr) != entry.buffers.end());
+        }
+    );
+}
+
+
+} // /namespace LLGL
+
+
+
+// ================================================================================

sources/Renderer/OpenGL/Command/GLCommandBuffer.cpp
@@ -7,6 +7,7 @@
 
 #include "GLCommandBuffer.h"
 #include "../Buffer/GLBufferWithXFB.h"
+#include "../Buffer/GLVertexArrayCache.h"
 #include "../RenderState/GLState.h"
 #include "../RenderState/GLPipelineLayout.h"
 #include "../RenderState/GLPipelineState.h"
@@ -55,6 +56,7 @@ void GLCommandBuffer::SetPipelineRenderState(const GLPipelineState& pipelineStat
         auto& graphicsPSO = LLGL_CAST(const GLGraphicsPSO&, pipelineStateGL);
         renderState_.drawMode       = graphicsPSO.GetDrawMode();
         renderState_.primitiveMode  = graphicsPSO.GetPrimitiveMode();
+        SetVertexInputLayout(graphicsPSO.GetVertexInputLayout());
     }
 
     /* Store barrier flags; These must be invalidated when a new resource or resource-heap is set */
@@ -135,6 +137,34 @@ GLbitfield GLCommandBuffer::FlushAndGetMemoryBarriers()
     GLbitfield barriers = renderState_.dirtyBarriers;
     renderState_.dirtyBarriers &= renderState_.implicitBarriers; // Only keep implicit barriers
     return barriers;
+}
+
+void GLCommandBuffer::SetVertexInputLayout(const GLVertexInputLayout& vertexInputLayout)
+{
+    if (vertexInputState_.vertexInputLayout.GetHash() != vertexInputLayout.GetHash())
+    {
+        vertexInputState_.dirtyBit          = true;
+        vertexInputState_.vertexInputLayout = vertexInputLayout;
+    }
+}
+
+void GLCommandBuffer::SetBufferInputLayout(const GLBufferInputLayout& bufferInputLayout)
+{
+    if (vertexInputState_.bufferInputLayout.GetHash() != bufferInputLayout.GetHash())
+    {
+        vertexInputState_.dirtyBit          = true;
+        vertexInputState_.bufferInputLayout = bufferInputLayout;
+    }
+}
+
+GLSharedContextVertexArray* GLCommandBuffer::FlushVertexInput()
+{
+    if (vertexInputState_.dirtyBit)
+    {
+        vertexInputState_.dirtyBit = false;
+        return GLVertexArrayCache::Get().GetVertexArray(vertexInputState_.vertexInputLayout, vertexInputState_.bufferInputLayout);
+    }
+    return nullptr;
 }
 
 /* ----- Extensions ----- */

sources/Renderer/OpenGL/Command/GLDeferredCommandBuffer.cpp
@@ -320,8 +320,7 @@ void GLDeferredCommandBuffer::SetVertexBuffer(Buffer& buffer)
     if ((buffer.GetBindFlags() & BindFlags::VertexBuffer) != 0)
     {
         auto& vertexBufferGL = LLGL_CAST(GLBufferWithVAO&, buffer);
-        auto cmd = AllocCommand<GLCmdBindVertexArray>(GLOpcodeBindVertexArray);
-        cmd->vertexArray = vertexBufferGL.GetVertexArray();
+        SetBufferInputLayout(&vertexBufferGL);
 
         #if LLGL_GLEXT_TRANSFORM_FEEDBACK2
         SetTransformFeedbackChecked(vertexBufferGL);
@@ -329,26 +328,12 @@ void GLDeferredCommandBuffer::SetVertexBuffer(Buffer& buffer)
     }
 }
 
-void GLDeferredCommandBuffer::SetVertexBuffer(Buffer& buffer, std::uint32_t numVertexAttribs, const VertexAttribute* vertexAttribs)
+void GLDeferredCommandBuffer::SetVertexBuffer(Buffer& buffer, std::uint32_t /*numVertexAttribs*/, const VertexAttribute* /*vertexAttribs*/)
 {
     if ((buffer.GetBindFlags() & BindFlags::VertexBuffer) != 0)
     {
         auto& vertexBufferGL = LLGL_CAST(GLBufferWithVAO&, buffer);
-
-        auto cmdBuildVertexArray = AllocCommand<GLCmdBuildVertexArray>(GLOpcodeBuildVertexArray, sizeof(GLVertexAttribute) * numVertexAttribs);
-        {
-            cmdBuildVertexArray->bufferWithVAO      = &vertexBufferGL;
-            cmdBuildVertexArray->numVertexAttribs   = numVertexAttribs;
-            auto* dstVertexAttribs = reinterpret_cast<GLVertexAttribute*>(cmdBuildVertexArray + 1);
-
-            for_range(i, numVertexAttribs)
-                GLConvertVertexAttrib(dstVertexAttribs[i], vertexAttribs[i], vertexBufferGL.GetID());
-        }
-
-        auto cmdBindVertexArray = AllocCommand<GLCmdBindVertexArray>(GLOpcodeBindVertexArray);
-        {
-            cmdBindVertexArray->vertexArray = vertexBufferGL.GetVertexArray();
-        }
+        SetBufferInputLayout(&vertexBufferGL);
 
         #if LLGL_GLEXT_TRANSFORM_FEEDBACK2
         SetTransformFeedbackChecked(vertexBufferGL);
@@ -361,8 +346,7 @@ void GLDeferredCommandBuffer::SetVertexBufferArray(BufferArray& bufferArray)
     if ((bufferArray.GetBindFlags() & BindFlags::VertexBuffer) != 0)
     {
         auto& bufferArrayWithVAO = LLGL_CAST(GLBufferArrayWithVAO&, bufferArray);
-        auto cmd = AllocCommand<GLCmdBindVertexArray>(GLOpcodeBindVertexArray);
-        cmd->vertexArray = bufferArrayWithVAO.GetVertexArray();
+        SetBufferInputLayout(bufferArrayWithVAO.GetInputLayout());
     }
 }
 
@@ -831,9 +815,16 @@ The indices actually store the index start offset, but must be passed to GL as a
 #   define LLGL_FLUSH_MEMORY_BARRIERS()
 #endif // /LLGL_GLEXT_MEMORY_BARRIERS
 
+#define LLGL_FLUSH_VERTEX_ARRAY() \
+    FlushBindVertexArray()
+
+#define LLGL_FLUSH_DRAW_COMMAND_STATES()    \
+    LLGL_FLUSH_VERTEX_ARRAY();              \
+    LLGL_FLUSH_MEMORY_BARRIERS()
+
 void GLDeferredCommandBuffer::Draw(std::uint32_t numVertices, std::uint32_t firstVertex)
 {
-    LLGL_FLUSH_MEMORY_BARRIERS();
+    LLGL_FLUSH_DRAW_COMMAND_STATES();
     auto cmd = AllocCommand<GLCmdDrawArrays>(GLOpcodeDrawArrays);
     {
         cmd->mode   = GetDrawMode();
@@ -844,7 +835,7 @@ void GLDeferredCommandBuffer::Draw(std::uint32_t numVertices, std::uint32_t firs
 
 void GLDeferredCommandBuffer::DrawIndexed(std::uint32_t numIndices, std::uint32_t firstIndex)
 {
-    LLGL_FLUSH_MEMORY_BARRIERS();
+    LLGL_FLUSH_DRAW_COMMAND_STATES();
     auto cmd = AllocCommand<GLCmdDrawElements>(GLOpcodeDrawElements);
     {
         cmd->mode       = GetDrawMode();
@@ -856,7 +847,7 @@ void GLDeferredCommandBuffer::DrawIndexed(std::uint32_t numIndices, std::uint32_
 
 void GLDeferredCommandBuffer::DrawIndexed(std::uint32_t numIndices, std::uint32_t firstIndex, std::int32_t vertexOffset)
 {
-    LLGL_FLUSH_MEMORY_BARRIERS();
+    LLGL_FLUSH_DRAW_COMMAND_STATES();
     auto cmd = AllocCommand<GLCmdDrawElementsBaseVertex>(GLOpcodeDrawElementsBaseVertex);
     {
         cmd->mode       = GetDrawMode();
@@ -869,7 +860,7 @@ void GLDeferredCommandBuffer::DrawIndexed(std::uint32_t numIndices, std::uint32_
 
 void GLDeferredCommandBuffer::DrawInstanced(std::uint32_t numVertices, std::uint32_t firstVertex, std::uint32_t numInstances)
 {
-    LLGL_FLUSH_MEMORY_BARRIERS();
+    LLGL_FLUSH_DRAW_COMMAND_STATES();
     auto cmd = AllocCommand<GLCmdDrawArraysInstanced>(GLOpcodeDrawArraysInstanced);
     {
         cmd->mode           = GetDrawMode();
@@ -882,7 +873,7 @@ void GLDeferredCommandBuffer::DrawInstanced(std::uint32_t numVertices, std::uint
 void GLDeferredCommandBuffer::DrawInstanced(std::uint32_t numVertices, std::uint32_t firstVertex, std::uint32_t numInstances, std::uint32_t firstInstance)
 {
     #ifndef __APPLE__
-    LLGL_FLUSH_MEMORY_BARRIERS();
+    LLGL_FLUSH_DRAW_COMMAND_STATES();
     auto cmd = AllocCommand<GLCmdDrawArraysInstancedBaseInstance>(GLOpcodeDrawArraysInstancedBaseInstance);
     {
         cmd->mode           = GetDrawMode();
@@ -898,7 +889,7 @@ void GLDeferredCommandBuffer::DrawInstanced(std::uint32_t numVertices, std::uint
 
 void GLDeferredCommandBuffer::DrawIndexedInstanced(std::uint32_t numIndices, std::uint32_t numInstances, std::uint32_t firstIndex)
 {
-    LLGL_FLUSH_MEMORY_BARRIERS();
+    LLGL_FLUSH_DRAW_COMMAND_STATES();
     auto cmd = AllocCommand<GLCmdDrawElementsInstanced>(GLOpcodeDrawElementsInstanced);
     {
         cmd->mode           = GetDrawMode();
@@ -911,7 +902,7 @@ void GLDeferredCommandBuffer::DrawIndexedInstanced(std::uint32_t numIndices, std
 
 void GLDeferredCommandBuffer::DrawIndexedInstanced(std::uint32_t numIndices, std::uint32_t numInstances, std::uint32_t firstIndex, std::int32_t vertexOffset)
 {
-    LLGL_FLUSH_MEMORY_BARRIERS();
+    LLGL_FLUSH_DRAW_COMMAND_STATES();
     auto cmd = AllocCommand<GLCmdDrawElementsInstancedBaseVertex>(GLOpcodeDrawElementsInstancedBaseVertex);
     {
         cmd->mode           = GetDrawMode();
@@ -926,7 +917,7 @@ void GLDeferredCommandBuffer::DrawIndexedInstanced(std::uint32_t numIndices, std
 void GLDeferredCommandBuffer::DrawIndexedInstanced(std::uint32_t numIndices, std::uint32_t numInstances, std::uint32_t firstIndex, std::int32_t vertexOffset, std::uint32_t firstInstance)
 {
     #ifndef __APPLE__
-    LLGL_FLUSH_MEMORY_BARRIERS();
+    LLGL_FLUSH_DRAW_COMMAND_STATES();
     auto cmd = AllocCommand<GLCmdDrawElementsInstancedBaseVertexBaseInstance>(GLOpcodeDrawElementsInstancedBaseVertexBaseInstance);
     {
         cmd->mode           = GetDrawMode();
@@ -944,7 +935,7 @@ void GLDeferredCommandBuffer::DrawIndexedInstanced(std::uint32_t numIndices, std
 
 void GLDeferredCommandBuffer::DrawIndirect(Buffer& buffer, std::uint64_t offset)
 {
-    LLGL_FLUSH_MEMORY_BARRIERS();
+    LLGL_FLUSH_DRAW_COMMAND_STATES();
     auto cmd = AllocCommand<GLCmdDrawArraysIndirect>(GLOpcodeDrawArraysIndirect);
     {
         cmd->id             = LLGL_CAST(GLBuffer&, buffer).GetID();
@@ -957,7 +948,7 @@ void GLDeferredCommandBuffer::DrawIndirect(Buffer& buffer, std::uint64_t offset)
 
 void GLDeferredCommandBuffer::DrawIndirect(Buffer& buffer, std::uint64_t offset, std::uint32_t numCommands, std::uint32_t stride)
 {
-    LLGL_FLUSH_MEMORY_BARRIERS();
+    LLGL_FLUSH_DRAW_COMMAND_STATES();
     #ifndef __APPLE__
     if (HasExtension(GLExt::ARB_multi_draw_indirect))
     {
@@ -987,7 +978,7 @@ void GLDeferredCommandBuffer::DrawIndirect(Buffer& buffer, std::uint64_t offset,
 
 void GLDeferredCommandBuffer::DrawIndexedIndirect(Buffer& buffer, std::uint64_t offset)
 {
-    LLGL_FLUSH_MEMORY_BARRIERS();
+    LLGL_FLUSH_DRAW_COMMAND_STATES();
     auto cmd = AllocCommand<GLCmdDrawElementsIndirect>(GLOpcodeDrawElementsIndirect);
     {
         cmd->id             = LLGL_CAST(GLBuffer&, buffer).GetID();
@@ -1001,7 +992,7 @@ void GLDeferredCommandBuffer::DrawIndexedIndirect(Buffer& buffer, std::uint64_t 
 
 void GLDeferredCommandBuffer::DrawIndexedIndirect(Buffer& buffer, std::uint64_t offset, std::uint32_t numCommands, std::uint32_t stride)
 {
-    LLGL_FLUSH_MEMORY_BARRIERS();
+    LLGL_FLUSH_DRAW_COMMAND_STATES();
     #ifndef __APPLE__
     if (HasExtension(GLExt::ARB_multi_draw_indirect))
     {
@@ -1033,7 +1024,7 @@ void GLDeferredCommandBuffer::DrawIndexedIndirect(Buffer& buffer, std::uint64_t 
 
 void GLDeferredCommandBuffer::DrawStreamOutput()
 {
-    LLGL_FLUSH_MEMORY_BARRIERS();
+    LLGL_FLUSH_DRAW_COMMAND_STATES();
     if (GLBufferWithXFB* bufferWithXfbGL = GetRenderState().boundBufferWithFxb)
     {
         #if LLGL_GLEXT_TRANSFORM_FEEDBACK2
@@ -1239,6 +1230,15 @@ void GLDeferredCommandBuffer::FlushMemoryBarriers()
         {
             cmd->barriers = barriers;
         }
+    }
+}
+
+void GLDeferredCommandBuffer::FlushBindVertexArray()
+{
+    if (GLSharedContextVertexArray* vertexArray = FlushVertexInput())
+    {
+        auto cmd = AllocCommand<GLCmdBindVertexArray>(GLOpcodeBindVertexArray);
+        cmd->vertexArray = vertexArray;
     }
 }
 

sources/Renderer/OpenGL/GLRenderSystem.cpp
@@ -20,6 +20,7 @@
 #include "Buffer/GLBufferWithVAO.h"
 #include "Buffer/GLBufferWithXFB.h"
 #include "Buffer/GLBufferArrayWithVAO.h"
+#include "Buffer/GLVertexArrayCache.h"
 #include "../CheckedCast.h"
 #include "../BufferUtils.h"
 #include "../TextureUtils.h"
@@ -76,6 +77,7 @@ GLRenderSystem::~GLRenderSystem()
     GLTextureViewPool::Get().Clear();
     GLMipGenerator::Get().Clear();
     GLStatePool::Get().Clear();
+    GLVertexArrayCache::Get().Clear();
 }
 
 /* ----- Swap-chain ----- */

sources/Renderer/OpenGL/Buffer/GL3PlusSharedContextVertexArray.cpp
@@ -31,10 +31,10 @@ void GL3PlusSharedContextVertexArray::BuildVertexLayout(const ArrayView<GLVertex
     inputLayout_.Append(attributes);
 }
 
-void GL3PlusSharedContextVertexArray::BuildVertexLayout(GLuint bufferID, const ArrayView<VertexAttribute>& attributes)
+/*void GL3PlusSharedContextVertexArray::BuildVertexLayout(GLuint bufferID, const ArrayView<VertexAttribute>& attributes)
 {
     inputLayout_.Append(bufferID, attributes);
-}
+}*/
 
 void GL3PlusSharedContextVertexArray::Finalize()
 {

sources/Renderer/OpenGL/Buffer/GL3PlusSharedContextVertexArray.h
@@ -35,7 +35,7 @@ class GL3PlusSharedContextVertexArray
 
         // Stores the vertex attributes for later use via glVertexAttrib*Pointer() functions.
         void BuildVertexLayout(const ArrayView<GLVertexAttribute>& attributes);
-        void BuildVertexLayout(GLuint bufferID, const ArrayView<VertexAttribute>& attributes);
+        //void BuildVertexLayout(GLuint bufferID, const ArrayView<VertexAttribute>& attributes);
 
         // Finalize the vertex array.
         void Finalize();

sources/Renderer/OpenGL/Buffer/GLBufferArrayWithVAO.cpp
@@ -10,15 +10,27 @@
 #include "../RenderState/GLStateManager.h"
 #include "../../CheckedCast.h"
 #include "../../../Core/CoreUtils.h"
+#include "../../../Core/Assertion.h"
 #include "../Ext/GLExtensionRegistry.h"
+#include <LLGL/Utils/ForRange.h>
 
 
 namespace LLGL
 {
 
 
+static std::vector<GLBuffer*> GetAsGLBuffers(ArrayView<Buffer*> inBuffers)
+{
+    std::vector<GLBuffer*> outBuffers;
+    outBuffers.resize(inBuffers.size());
+    for_range(i, inBuffers.size())
+        outBuffers[i] = LLGL_CAST(GLBuffer*, inBuffers[i]);
+    return outBuffers;
+}
+
 GLBufferArrayWithVAO::GLBufferArrayWithVAO(std::uint32_t numBuffers, Buffer* const * bufferArray) :
-    GLBufferArray { numBuffers, bufferArray }
+    GLBufferArray      { numBuffers, bufferArray                                       },
+    bufferInputLayout_ { GetAsGLBuffers(ArrayView<Buffer*>{ bufferArray, numBuffers }) }
 {
     /* Build vertex array and finalize afterwards as it references multiple buffers */
     while (GLBuffer* bufferGL = NextArrayResource<GLBuffer>(numBuffers, bufferArray))

sources/Renderer/OpenGL/Buffer/GLBufferArrayWithVAO.h
@@ -11,6 +11,7 @@
 
 #include "GLBufferArray.h"
 #include "GLSharedContextVertexArray.h"
+#include "GLBufferInputLayout.h"
 
 
 namespace LLGL
@@ -34,9 +35,16 @@ class GLBufferArrayWithVAO final : public GLBufferArray
             return &vertexArray_;
         }
 
+        // Returns the GL buffer input layout with hash over all buffers.
+        inline const GLBufferInputLayout& GetInputLayout() const
+        {
+            return bufferInputLayout_;
+        }
+
     private:
 
-        GLSharedContextVertexArray vertexArray_;
+        GLSharedContextVertexArray  vertexArray_; //DEPRECATED
+        GLBufferInputLayout         bufferInputLayout_;
 
 };
 

commit 04d408a42a27898a26d181c91a02c6cafd7bf72a

[GL] Fixed vertex binding issues introduced with 8c8cf40e.

sources/Renderer/OpenGL/Buffer/GLVertexArrayCache.cpp
@@ -36,14 +36,9 @@ void GLVertexArrayCache::Clear()
 
 GLSharedContextVertexArray* GLVertexArrayCache::GetVertexArray(const GLVertexInputLayout& vertexInputLayout, const GLBufferInputLayout& bufferInputLayout)
 {
+    /* Always return a VAO, even when there are no input attributes since GL always needs a bound VAO for drawing */
     const ArrayView<GLVertexAttribute>  attribs = vertexInputLayout.GetAttribs();
     const ArrayView<GLBuffer*>          buffers = bufferInputLayout.GetBuffers();
-
-    if (attribs.empty() || vertexInputLayout.GetHash() == 0 ||
-        buffers.empty() || bufferInputLayout.GetHash() == 0)
-    {
-        return nullptr;
-    }
 
     /* Get combination of vertex input and buffer input hashes */
     std::size_t combinedHash = 0;

sources/Renderer/OpenGL/Command/GLCommandBuffer.cpp
@@ -23,9 +23,14 @@ namespace LLGL
 
 void GLCommandBuffer::ResetRenderState()
 {
+    /* Reset pointers to bound pipeline objects */
     renderState_.boundPipelineLayout    = nullptr;
     renderState_.boundPipelineState     = nullptr;
     renderState_.boundBufferWithFxb     = nullptr;
+
+    /* Reset vertex input state to ensure VAOs are bound correctly */
+    vertexInputState_.vertexInputLayout.Reset();
+    vertexInputState_.bufferInputLayout.Reset();
 }
 
 void GLCommandBuffer::SetIndexFormat(bool indexType16Bits, std::uint64_t offset)

sources/Renderer/OpenGL/Command/GLDeferredCommandBuffer.cpp
@@ -1011,9 +1011,9 @@ void GLDeferredCommandBuffer::DrawIndexedIndirect(Buffer& buffer, std::uint64_t 
 
 void GLDeferredCommandBuffer::DrawStreamOutput()
 {
-    LLGL_FLUSH_DRAW_COMMAND_STATES();
     if (GLBufferWithXFB* bufferWithXfbGL = GetRenderState().boundBufferWithFxb)
     {
+        LLGL_FLUSH_DRAW_COMMAND_STATES();
         #if LLGL_GLEXT_TRANSFORM_FEEDBACK2
         if (HasExtension(GLExt::ARB_transform_feedback2))
         {

sources/Renderer/OpenGL/GLRenderSystem.cpp
@@ -697,6 +697,7 @@ static void GLQueryRendererInfo(RendererInfo& info)
 
     const std::set<const char*>& extensionNames = GetLoadedOpenGLExtensions();
     info.extensionNames = std::vector<UTF8String>(extensionNames.begin(), extensionNames.end());
+    std::sort(info.extensionNames.begin(), info.extensionNames.end());
 
     GLQueryPipelineCacheID(info.pipelineCacheID);
 }

sources/Renderer/OpenGL/Profile/GLCore/GLCoreExtensionLoader.cpp
@@ -1007,6 +1007,12 @@ static void IncludeImpliedExtensions(GLExtensionMap& extensions)
     };
     ImplyExtension("GL_ARB_gpu_shader5", { "GL_ARB_geometry_shader4" });
     ImplyExtension("GL_ARB_occlusion_query2", { "GL_ARB_occlusion_query" });
+
+    /*
+    This is a workaround, GL_ARB_transform_feedback2 does *not* imply support for GL_EXT_transform_feedback,
+    but since this extension is already loaded without the EXT suffixes, simply enable this extension
+    */
+    ImplyExtension("GL_ARB_transform_feedback2", { "GL_EXT_transform_feedback" });
 }
 
 #endif // /__APPLE__

sources/Renderer/OpenGL/RenderState/GLStateManager.cpp
@@ -1757,6 +1757,9 @@ void GLStateManager::BindRenderTarget(RenderTarget& renderTarget, GLStateManager
     }
 }
 
+//TODO:
+// `GL_RASTERIZER_DISCARD` must also be taken into account! From OpenGL man pages:
+// "GL_RASTERIZER_DISCARD [...] when enabled, glClear, glClearBufferData, glClearBufferSubData, glClearTexImage, and glClearTexSubImage are ignored."
 void GLStateManager::Clear(long flags)
 {
     GLFramebufferClearState clearState;

sources/Renderer/OpenGL/Shader/GLShaderProgram.cpp
@@ -1090,7 +1090,6 @@ void GLShaderProgram::BuildProgramBinary(
     /* Build input layout for vertex shader */
     LinearStringContainer attribNames;
 
-#if 0
     if (!inputVertexAttribs.empty())
     {
         std::vector<GLShaderAttribute> inputGLVertexAttribs;
@@ -1098,7 +1097,6 @@ void GLShaderProgram::BuildProgramBinary(
         GLShaderProgram::BindAttribLocations(GetID(), inputGLVertexAttribs);
     }
     else
-#endif
     {
         // Deprecated
         if (const GLShader* vs = orderedShaders.vertexShader)
@@ -1110,7 +1108,6 @@ void GLShaderProgram::BuildProgramBinary(
         GLShaderProgram::BindFragDataLocations(GetID(), fs->GetFragmentAttribs());
 
     /* Build transform feedback varyings for vertex or geometry shader and link program */
-#if 0
     if (!outputVertexAttribs.empty())
     {
         std::vector<const char*> varyings;
@@ -1118,7 +1115,6 @@ void GLShaderProgram::BuildProgramBinary(
         GLShaderProgram::LinkProgramWithTransformFeedbackVaryings(GetID(), varyings);
     }
     else
-#endif
     {
         // Deprecated
         const GLShader* shaderWithVaryings = nullptr;